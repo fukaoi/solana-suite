c99ddb85b705e634d7fcc88546232e7ac00756b7
fukaoi <goodakira7@gmail.com>
2021-09-09 10:45:08 +0900

    using main purpose of test, example integration

--- src/util.ts
+++ src/util.ts
@@ -27,4 +27,13 @@ export namespace Util {
     const decoded = bs.decode(secret);
     return Keypair.fromSecretKey(decoded);
   }
+  export const dateFormat = (): string => {
+    const t = new Date();
+    return t.getFullYear() + '-' +
+      ("0" + (t.getMonth() + 1)).slice(-2) + '-' +
+      ("0" + (t.getDate())).slice(-2) + '/' +
+      ("0" + t.getHours()).slice(-2) + ':' +
+      ("0" + t.getMinutes()).slice(-2) + ':' +
+      ("0" + t.getSeconds()).slice(-2);
+  }
 }
--- test/exmaples/integration1-metaplex-nft.test.ts
+++ test/exmaples/integration1-metaplex-nft.test.ts
@@ -1,33 +1,42 @@
 import {describe, it} from 'mocha';
-import {expect, assert} from 'chai'
 import {Wallet} from '../../src/wallet';
+import {Util} from '../../src/util';
 import {Transaction} from '../../src/transaction';
 import {MetaplexMint} from '../../src/nft/metaplex/mint';
 import {MetaplexObject} from '../../src/nft/metaplex/object';
 import {MetaplexMetaData} from '../../src/nft/metaplex/metadata';
 import {StorageNftStorage} from '../../src/nft/storage/nft-storage';
 
-describe('### Integration1 ###', () => {
+describe('##### Integration1 #####', () => {
   it('Create Metaplex NFT, transfer NFT', async () => {
+
+    //////////////////////////////////////////////
+    // CREATE WALLET 
+    //////////////////////////////////////////////
+
     // create nft owner wallet, receive nft receipt wallet.
     const owner = await Wallet.create();
-    const receipt = await Wallet.create(); 
+    const receipt = await Wallet.create();
+
+    console.log('# owner: ', owner);
+    console.log('# receipt: ', receipt);
 
 
-    ///////////////////////////////////////////////
+    //////////////////////////////////////////////
     // CREATE NFT, MINT NFT FROM THIS LINE 
     //////////////////////////////////////////////
 
     // optional metadata and image upload
+    const name = `Cat:${Util.dateFormat()}`;
     const url = await StorageNftStorage.upload(
-      'Cat',
+      name,
       'Cute cat is like',
       'test/nft/storage/cat.jpeg'
     );
 
     // set for save metadata in blockchain[preprocessing]
     const metadata = new MetaplexObject.Data({
-      name: 'Cat',
+      name,
       symbol: 'CAT',
       uri: url,
       sellerFeeBasisPoints: 100,
@@ -37,7 +46,10 @@ describe('### Integration1 ###', () => {
     // create nft by owner.
     // owner.pubkey: transaction payer
     // [owner.secret]: signer.enable multi signature for array param
-    const txsign = await MetaplexMint.create(owner.pubkey, [owner.secret])();
+    const txsign = await MetaplexMint.create(
+      owner.pubkey,
+      [owner.secret]
+    )();
 
     // this is NFT ID
     console.log('# mintKey: ', txsign.mintKey);
@@ -53,16 +65,24 @@ describe('### Integration1 ###', () => {
     // txsign.signers: all signers that need to send transaction
     // tx: txsign => tx be relied
     const mintSig = await Transaction.sendInstructions(
-      txsign.signers, 
+      txsign.signers,
       tx,
     );
     console.log('# Create nft sig: ', mintSig);
 
 
-    ///////////////////////////////////////////////
+    //////////////////////////////////////////////
     // TRANSFER RECEIPR USER FROM THIS LINE 
     //////////////////////////////////////////////
-    
 
+    // transfer nft to receipt wallet
+    // const transferSig = await SplNft.transferNft(
+      // txsign.mintKey,
+      // owner.secret,
+      // receipt.pubkey
+    // );
+
+    // console.log('# Transfer nft sig: ', transferSig);
   });
 })
+
