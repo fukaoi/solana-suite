9a093b139572adc9146efca492db572ce2e23882
fukaoi <goodakira7@gmail.com>
2021-09-15 23:43:13 +0900

    updated comment

--- src/constants.ts
+++ src/constants.ts
@@ -27,7 +27,8 @@ export namespace Constants {
   export const MEMO_PROGRAM_ID = 'Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo';
   export const METAPLEX_PROGRAM_ID = 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s';
   export const COMMITMENT: Commitment = 'singleGossip';
-  //todo: this NFT_STORAGE_API_KEY moved .env file
+  // todo: this NFT_STORAGE_API_KEY moved .env file
+  // NFT.storage can store NFTs up to 32GB in size!
   export const NFT_STORAGE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweERGMjcyN2VkODZhRGU1RTMyZDZDZEJlODc0YzRFNDlEODY1OWZmOEMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYyMDI2NDk0MzcwNiwibmFtZSI6ImRlbW8ifQ.d4J70mikxRB8a5vwNu6SO5HDA8JaueuseAj7Q_ytMCE';
 }
 
