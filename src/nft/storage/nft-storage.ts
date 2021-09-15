10ffb1ed4a9961d6b675021d67d8a18696f35a71
fukaoi <goodakira7@gmail.com>
2021-09-15 23:21:02 +0900

    Moeved constant file

--- src/nft/storage/nft-storage.ts
+++ src/nft/storage/nft-storage.ts
@@ -1,8 +1,8 @@
 import {NFTStorage} from 'nft.storage';
 import fs from 'fs';
+import {Constants} from '../../constants';
 
 export namespace StorageNftStorage {
-  const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweERGMjcyN2VkODZhRGU1RTMyZDZDZEJlODc0YzRFNDlEODY1OWZmOEMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYyMDI2NDk0MzcwNiwibmFtZSI6ImRlbW8ifQ.d4J70mikxRB8a5vwNu6SO5HDA8JaueuseAj7Q_ytMCE';
 
   const createGatewayUrl = (cid: string): string => `https://ipfs.io/ipfs/${cid}`
 
@@ -11,7 +11,7 @@ export namespace StorageNftStorage {
     description: string,
     imagePath: string
   ): Promise<string> => {
-    const client = new NFTStorage({token: apiKey});
+    const client = new NFTStorage({token: Constants.NFT_STORAGE_API_KEY});
     const cid = await client.storeBlob(await fs.promises.readFile(imagePath));
     const url = createGatewayUrl(cid);
 
