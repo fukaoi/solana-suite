09213552d9ab4ab1712b23b7eeb984e38116e073
fukaoi <goodakira7@gmail.com>
2021-09-29 22:34:27 +0900

    Updated lib/

--- lib/nft/storage/index.d.ts
+++ lib/nft/storage/index.d.ts
@@ -1,7 +1,34 @@
+export interface Attributes {
+}
+export interface Collection {
+}
+export interface Properties {
+}
+export interface Creators {
+}
+export interface MetadataFormat {
+    name: string;
+    uri: string;
+    symbol: string;
+    update_authority: string;
+    creators?: Creators[];
+    seller_fee_basis_points?: number;
+    primary_sale_happened?: boolean;
+}
+export interface MetadataStorageFormat {
+    name: string;
+    description: string;
+    image: string;
+    symbol?: string;
+    seller_fee_basis_points?: number;
+    animation_url?: string;
+    external_url?: string;
+    category?: string;
+    attributes?: Attributes[];
+    collection?: Collection;
+    properties?: Properties[];
+    creators?: Creators[];
+}
 export declare namespace Storage {
-    interface MetadataFormat {
-        name: string;
-        description: string;
-        image: string;
-    }
+    const initStorageData: () => MetadataStorageFormat;
 }
