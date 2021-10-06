56abbf728a6faf56b3d7f59a92204c1931eaa9f7
fukaoi <goodakira7@gmail.com>
2021-10-06 10:31:15 +0900

    added function that create explorer uri + signature

--- src/constants.ts
+++ src/constants.ts
@@ -5,36 +5,13 @@ import {
 import './util';
 import './global';
 
-export namespace ConstantsFunc {
-  export const switchApi = (env: string | undefined) => {
-    switch (env) {
-      case 'production':
-        return 'https://api.solana.com';
-      case 'test':
-        return 'https://api.testnet.solana.com';
-      default:
-        process.env.NODE_ENV = 'development';
-        return 'http://api.devnet.solana.com';
-    }
-  }
-
-  export const switchNetwork = (env: string | undefined) => {
-    switch (env) {
-      case 'production':
-        return 'mainnet';
-      case 'test':
-        return 'testnet';
-      default:
-        process.env.NODE_ENV = 'development';
-        return 'devnet';
-    }
-  }
-
-  export const swtichArweaveUpload = () =>
-    'https://us-central1-principal-lane-200702.cloudfunctions.net/uploadFile4';
-}
-
 export namespace Constants {
+  export const ENV = {
+    prd: 'production',
+    dev: 'development',
+    test: 'test',
+  }
+
   export const CURRENT_NETWORK = ConstantsFunc.switchApi(process.env.NODE_ENV);
   export const API_URL = ConstantsFunc.switchApi(process.env.NODE_ENV);
   export const SYSTEM_PROGRAM_ID = '11111111111111111111111111111111'.toPubKey();
@@ -48,10 +25,36 @@ export namespace Constants {
   export const NFT_STORAGE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweERGMjcyN2VkODZhRGU1RTMyZDZDZEJlODc0YzRFNDlEODY1OWZmOEMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYyMDI2NDk0MzcwNiwibmFtZSI6ImRlbW8ifQ.d4J70mikxRB8a5vwNu6SO5HDA8JaueuseAj7Q_ytMCE';
 
   export const NFT_STORAGE_GATEWAY_URL = 'https://ipfs.io/ipfs';
-  export const ARWEAVE_UPLOAD_SRV_URL = ConstantsFunc.swtichArweaveUpload();
+  export const ARWEAVE_UPLOAD_SRV_URL = 'https://us-central1-principal-lane-200702.cloudfunctions.net/uploadFile4';
   export const ARWEAVE_GATEWAY_URL = 'https://arweave.net';
   export const AR_SOL_HOLDER_ID = 'HvwC9QSAzvGXhhVrgPmauVwFWcYZhne3hVot9EbHuFTm'.toPubKey();
 
-  export const COIN_MARKET_URL =  'https://api.coingecko.com/api/v3/simple/price';
+  export const COIN_MARKET_URL = 'https://api.coingecko.com/api/v3/simple/price';
 }
 
+export namespace ConstantsFunc {
+  export const switchApi = (env: string | undefined) => {
+    console.log(Constants);
+    switch (env) {
+      case Constants.ENV.prd:
+        return 'https://api.solana.com';
+      case Constants.ENV.test:
+        return 'https://api.testnet.solana.com';
+      default:
+        process.env.NODE_ENV = Constants.ENV.dev;
+        return 'http://api.devnet.solana.com';
+    }
+  }
+
+  export const switchNetwork = (env: string | undefined) => {
+    switch (env) {
+      case Constants.ENV.prd:
+        return 'mainnet';
+      case Constants.ENV.test:
+        return 'testnet';
+      default:
+        process.env.NODE_ENV = Constants.ENV.dev;
+        return 'devnet';
+    }
+  }
+}
--- src/global.ts
+++ src/global.ts
@@ -4,11 +4,13 @@ import {
 } from '@solana/web3.js';
 
 import bs from 'bs58';
+import {Constants, ConstantsFunc} from './constants';
 
 declare global {
   interface String {
     toPubKey(): PublicKey;
     toKeypair(): Keypair;
+    toSigUrl(): string;
   }
 }
 
@@ -21,4 +23,14 @@ String.prototype.toKeypair = function () {
   return Keypair.fromSecretKey(decoded);
 }
 
+String.prototype.toSigUrl = function () {
+  return `https://explorer.solana.com/tx/${this}?cluster=${ConstantsFunc.switchNetwork(process.env.NODE_ENV)}`;
+}
+
+console.debug = (data: any, data2: any = '') => {
+  if (process.env.NODE_ENV === Constants.ENV.dev
+    || process.env.NODE_ENV === Constants.ENV.test) {
+    console.log(`\u001b[35m${data}`, `\u001b[36m${data2}`);
+  }
+}
 
