18d11dfd70771668903b25091db2bdbacc950a59
fukaoi <goodakira7@gmail.com>
2021-10-06 10:40:30 +0900

    updated

--- src/constants.ts
+++ src/constants.ts
@@ -11,30 +11,10 @@ export namespace Constants {
     dev: 'development',
     test: 'test',
   }
-
-  export const CURRENT_NETWORK = ConstantsFunc.switchApi(process.env.NODE_ENV);
-  export const API_URL = ConstantsFunc.switchApi(process.env.NODE_ENV);
-  export const SYSTEM_PROGRAM_ID = '11111111111111111111111111111111'.toPubKey();
-  export const SPL_TOKEN_PROGRAM_ID = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'.toPubKey();
-  export const SPL_ASSOCIATED_TOKEN_PROGRAM_ID = 'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL'.toPubKey();
-  export const MEMO_PROGRAM_ID = 'Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo'.toPubKey();
-  export const METAPLEX_PROGRAM_ID = 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s'.toPubKey();
-  export const COMMITMENT: Commitment = 'singleGossip';
-  // todo: this NFT_STORAGE_API_KEY moved .env file
-  // NFT.storage can store NFTs up to 32GB in size!
-  export const NFT_STORAGE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweERGMjcyN2VkODZhRGU1RTMyZDZDZEJlODc0YzRFNDlEODY1OWZmOEMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYyMDI2NDk0MzcwNiwibmFtZSI6ImRlbW8ifQ.d4J70mikxRB8a5vwNu6SO5HDA8JaueuseAj7Q_ytMCE';
-
-  export const NFT_STORAGE_GATEWAY_URL = 'https://ipfs.io/ipfs';
-  export const ARWEAVE_UPLOAD_SRV_URL = 'https://us-central1-principal-lane-200702.cloudfunctions.net/uploadFile4';
-  export const ARWEAVE_GATEWAY_URL = 'https://arweave.net';
-  export const AR_SOL_HOLDER_ID = 'HvwC9QSAzvGXhhVrgPmauVwFWcYZhne3hVot9EbHuFTm'.toPubKey();
-
-  export const COIN_MARKET_URL = 'https://api.coingecko.com/api/v3/simple/price';
 }
 
 export namespace ConstantsFunc {
   export const switchApi = (env: string | undefined) => {
-    console.log(Constants);
     switch (env) {
       case Constants.ENV.prd:
         return 'https://api.solana.com';
@@ -57,4 +37,25 @@ export namespace ConstantsFunc {
         return 'devnet';
     }
   }
+}
+
+export namespace Constants {
+  export const CURRENT_NETWORK = ConstantsFunc.switchApi(process.env.NODE_ENV);
+  export const API_URL = ConstantsFunc.switchApi(process.env.NODE_ENV);
+  export const SYSTEM_PROGRAM_ID = '11111111111111111111111111111111'.toPubKey();
+  export const SPL_TOKEN_PROGRAM_ID = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'.toPubKey();
+  export const SPL_ASSOCIATED_TOKEN_PROGRAM_ID = 'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL'.toPubKey();
+  export const MEMO_PROGRAM_ID = 'Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo'.toPubKey();
+  export const METAPLEX_PROGRAM_ID = 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s'.toPubKey();
+  export const COMMITMENT: Commitment = 'singleGossip';
+  // todo: this NFT_STORAGE_API_KEY moved .env file
+  // NFT.storage can store NFTs up to 32GB in size!
+  export const NFT_STORAGE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweERGMjcyN2VkODZhRGU1RTMyZDZDZEJlODc0YzRFNDlEODY1OWZmOEMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYyMDI2NDk0MzcwNiwibmFtZSI6ImRlbW8ifQ.d4J70mikxRB8a5vwNu6SO5HDA8JaueuseAj7Q_ytMCE';
+
+  export const NFT_STORAGE_GATEWAY_URL = 'https://ipfs.io/ipfs';
+  export const ARWEAVE_UPLOAD_SRV_URL = 'https://us-central1-principal-lane-200702.cloudfunctions.net/uploadFile4';
+  export const ARWEAVE_GATEWAY_URL = 'https://arweave.net';
+  export const AR_SOL_HOLDER_ID = 'HvwC9QSAzvGXhhVrgPmauVwFWcYZhne3hVot9EbHuFTm'.toPubKey();
+
+  export const COIN_MARKET_URL = 'https://api.coingecko.com/api/v3/simple/price';
 }
