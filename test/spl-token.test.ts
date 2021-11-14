68305ef99acccd1fd824999a8f17cc342cda1574
fukaoi <goodakira7@gmail.com>
2021-11-14 13:27:25 +0900

    updated

--- test/spl-token.test.ts
+++ test/spl-token.test.ts
@@ -1,7 +1,7 @@
 import {describe, it} from 'mocha';
 import {assert} from 'chai';
 import {Setup} from '../test/utils/setup';
-import {Wallet, SplToken, Util, Multisig, Instruction} from '../src/'
+import {Wallet, SplToken, Multisig} from '../src/'
 
 let source: Wallet.KeypairStr;
 let dest: Wallet.KeypairStr;
@@ -51,23 +51,23 @@ describe('SplToken', () => {
     });
   });
 
-  // it('Create token', async () => {
-    // const TOKEN_TOTAL_AMOUNT = 10000000;
-    // const inst =
-      // await SplToken.mint(
-        // source.pubkey.toPubKey(),
-        // [source.secret.toKeypair()],
-        // TOKEN_TOTAL_AMOUNT,
-        // MINT_DECIMAL
-      // );
+  it('Create token', async () => {
+    const TOKEN_TOTAL_AMOUNT = 10000000;
+    const inst =
+      await SplToken.mint(
+        source.pubkey.toPubKey(),
+        [source.secret.toKeypair()],
+        TOKEN_TOTAL_AMOUNT,
+        MINT_DECIMAL
+      );
 
-    // assert.isTrue(inst.isOk, `${inst.unwrap()}`);
+    assert.isTrue(inst.isOk, `${inst.unwrap()}`);
 
-    // const res = await inst.unwrap().instruction.submit();
-    // assert.isTrue(res.isOk, res.unwrap());
-    // tokenKeyStr = inst.unwrap().tokenKey;
-    // console.log('# tokenKey: ', tokenKeyStr);
-  // });
+    const res = await inst.unwrap().submit();
+    assert.isTrue(res.isOk, res.unwrap().sig);
+    tokenKeyStr = inst.unwrap().value as string;
+    console.log('# tokenKey: ', tokenKeyStr);
+  });
 
   it.only('[Err]lack signer for multisig', async () => {
     const signer1 = Wallet.create();
@@ -92,158 +92,158 @@ describe('SplToken', () => {
       MINT_DECIMAL,
     );
     const res = await [
-      multisig.unwrap(), 
-      mint.unwrap().instruction
+      multisig.unwrap(),
+      mint.unwrap()
     ].submit();
 
-    assert.isTrue(res.isErr);
+    // assert.isTrue(res.isErr);
   });
 
-    // it('Create token, batch transfer', async () => {
-    // const TOKEN_TOTAL_AMOUNT = 10000000;
-    // const res =
-    // await SplToken.mint(
-    // source.pubkey.toPubKey(),
-    // [
-    // source.secret.toKeypair(),
-    // ],
-    // TOKEN_TOTAL_AMOUNT,
-    // MINT_DECIMAL,
-    // )();
+  it('Create token, batch transfer', async () => {
+    const TOKEN_TOTAL_AMOUNT = 10000000;
+    const mint =
+      await SplToken.mint(
+        source.pubkey.toPubKey(),
+        [
+          source.secret.toKeypair(),
+        ],
+        TOKEN_TOTAL_AMOUNT,
+        MINT_DECIMAL,
+      );
 
-    // assert.isTrue(res.isOk, res.unwrap());
-    // console.log('# tokenKey: ', res.unwrap());
-    // const token = res.unwrap();
+    assert.isTrue(mint.isOk, `${mint.unwrap()}`);
+    const token = mint.unwrap().value as string;
+    console.log('# tokenKey: ', token);
 
-    // const inst1 = await SplToken.transfer(
-    // token.toPubKey(),
-    // source.pubkey.toPubKey(),
-    // dest.pubkey.toPubKey(),
-    // [
-    // source.secret.toKeypair(),
-    // ],
-    // 1,
-    // MINT_DECIMAL,
-    // source.secret.toKeypair(),
-    // );
-    // assert.isTrue(inst1.isOk);
-
-    // const inst2 = await SplToken.transfer(
-    // token.toPubKey(),
-    // source.pubkey.toPubKey(),
-    // dest.pubkey.toPubKey(),
-    // [
-    // source.secret.toKeypair(),
-    // ],
-    // 1,
-    // MINT_DECIMAL,
-    // source.secret.toKeypair(),
-    // );
-    // assert.isTrue(inst2.isOk);
-
-    // const sig = await [inst1.unwrap(), inst2.unwrap()].submit();
-    // assert.isTrue(sig.isOk, sig.unwrap());
-    // console.log('signature: ', sig.unwrap().toSigUrl());
-    // });
-
-    // it('Create token, transfer with multisig and fee payer', async () => {
-    // const signer1 = Wallet.create();
-    // const signer2 = Wallet.create();
-    // const multisig = await Multisig.create(
-    // 2,
-    // source.secret.toKeypair(),
-    // [signer1.pubkey.toPubKey(), signer2.pubkey.toPubKey()]
-    // );
-
-    // assert.isTrue(multisig.isOk, `${multisig.unwrap()}`);
-
-    // const TOKEN_TOTAL_AMOUNT = 10000000;
-    // const res =
-    // await SplToken.mint(
-    // multisig.unwrap().multisig.toPubKey(),
-    // [
-    // source.secret.toKeypair(),
-    // signer1.secret.toKeypair(),
-    // signer2.secret.toKeypair(),
-    // ],
-    // TOKEN_TOTAL_AMOUNT,
-    // MINT_DECIMAL,
-    // )({
-    // feePayer: source.pubkey.toPubKey(),
-    // multiSig: multisig.unwrap().multisig.toPubKey()
-    // });
-
-    // assert.isTrue(res.isOk, res.unwrap());
-    // const token = res.unwrap();
-    // const inst = await SplToken.transfer(
-    // token.toPubKey(),
-    // multisig.unwrap().multisig.toPubKey(),
-    // dest.pubkey.toPubKey(),
-    // [
-    // signer1.secret.toKeypair(),
-    // signer2.secret.toKeypair(),
-    // ],
-    // 1,
-    // MINT_DECIMAL,
-    // source.secret.toKeypair(),
-    // );
-    // assert.isTrue(inst.isOk, `${inst.unwrap()}`);
-    // const sig = await inst.unwrap().submit();
-    // console.log('signature: ', `${sig.unwrap().toSigUrl()}`);
-    // });
-
-    // it('Create token with fee payer', async () => {
-    // const owner = Wallet.create();
-    // const feePayer = source;
-    // const before = (await Wallet.getBalance(feePayer.pubkey.toPubKey())).unwrap();
-    // const TOKEN_TOTAL_AMOUNT = 10000000;
-    // const res =
-    // await SplToken.mint(
-    // owner.pubkey.toPubKey(),
-    // [
-    // feePayer.secret.toKeypair(),
-    // owner.secret.toKeypair(),
-    // ],
-    // TOKEN_TOTAL_AMOUNT,
-    // MINT_DECIMAL
-    // )({
-    // feePayer: feePayer.pubkey.toPubKey()
-    // });
-    // assert.isTrue(res.isOk, res.unwrap());
-    // const after = (await Wallet.getBalance(feePayer.pubkey.toPubKey())).unwrap();
-    // console.log('# tokenKey: ', res.unwrap());
-    // assert.isTrue(before > after, `before fee: ${before}, after fee: ${after}`);
-    // });
-
-    // it('Subscribe a account(pubkey)', async () => {
-    // const subscribeId = SplToken.subscribeAccount(
-    // dest.pubkey.toPubKey(),
-    // (v: SplToken.TransferHistory) => {
-    // console.log('# Subscribe result: ', v);
-    // assert.isNotEmpty(v.type);
-    // assert.isNotNull(v.date);
-    // assert.isNotNull(v.info.mint);
-    // assert.isNotEmpty(v.info.source);
-    // assert.isNotEmpty(v.info.destination);
-    // }
-    // );
-    // for (let i = 0; i < 3; i++) await sendContinuously();
-    // await Util.sleep(15);
-    // SplToken.unsubscribeAccount(subscribeId);
-    // assert.ok('success subscribe');
-    // });
-  })
-
-  const sendContinuously = async (): Promise<void> => {
-    const inst = await SplToken.transfer(
-      tokenKeyStr.toPubKey(),
+    const inst1 = await SplToken.transfer(
+      token.toPubKey(),
       source.pubkey.toPubKey(),
       dest.pubkey.toPubKey(),
-      [source.secret.toKeypair()],
+      [
+        source.secret.toKeypair(),
+      ],
       1,
-      MINT_DECIMAL
+      MINT_DECIMAL,
+      source.secret.toKeypair(),
     );
-    inst.isOk && inst.value.submit();
-  }
+    assert.isTrue(inst1.isOk);
+
+    const inst2 = await SplToken.transfer(
+      token.toPubKey(),
+      source.pubkey.toPubKey(),
+      dest.pubkey.toPubKey(),
+      [
+        source.secret.toKeypair(),
+      ],
+      1,
+      MINT_DECIMAL,
+      source.secret.toKeypair(),
+    );
+    assert.isTrue(inst2.isOk);
+
+    const sig = await [inst1.unwrap(), inst2.unwrap()].submit();
+    assert.isTrue(sig.isOk, sig.unwrap().sig);
+    console.log('signature: ', sig.unwrap().sig.toSigUrl());
+  });
+
+  // it('Create token, transfer with multisig and fee payer', async () => {
+  // const signer1 = Wallet.create();
+  // const signer2 = Wallet.create();
+  // const multisig = await Multisig.create(
+  // 2,
+  // source.secret.toKeypair(),
+  // [signer1.pubkey.toPubKey(), signer2.pubkey.toPubKey()]
+  // );
+
+  // assert.isTrue(multisig.isOk, `${multisig.unwrap()}`);
+
+  // const TOKEN_TOTAL_AMOUNT = 10000000;
+  // const res =
+  // await SplToken.mint(
+  // multisig.unwrap().multisig.toPubKey(),
+  // [
+  // source.secret.toKeypair(),
+  // signer1.secret.toKeypair(),
+  // signer2.secret.toKeypair(),
+  // ],
+  // TOKEN_TOTAL_AMOUNT,
+  // MINT_DECIMAL,
+  // )({
+  // feePayer: source.pubkey.toPubKey(),
+  // multiSig: multisig.unwrap().multisig.toPubKey()
+  // });
+
+  // assert.isTrue(res.isOk, res.unwrap());
+  // const token = res.unwrap();
+  // const inst = await SplToken.transfer(
+  // token.toPubKey(),
+  // multisig.unwrap().multisig.toPubKey(),
+  // dest.pubkey.toPubKey(),
+  // [
+  // signer1.secret.toKeypair(),
+  // signer2.secret.toKeypair(),
+  // ],
+  // 1,
+  // MINT_DECIMAL,
+  // source.secret.toKeypair(),
+  // );
+  // assert.isTrue(inst.isOk, `${inst.unwrap()}`);
+  // const sig = await inst.unwrap().submit();
+  // console.log('signature: ', `${sig.unwrap().toSigUrl()}`);
+  // });
+
+  // it('Create token with fee payer', async () => {
+  // const owner = Wallet.create();
+  // const feePayer = source;
+  // const before = (await Wallet.getBalance(feePayer.pubkey.toPubKey())).unwrap();
+  // const TOKEN_TOTAL_AMOUNT = 10000000;
+  // const res =
+  // await SplToken.mint(
+  // owner.pubkey.toPubKey(),
+  // [
+  // feePayer.secret.toKeypair(),
+  // owner.secret.toKeypair(),
+  // ],
+  // TOKEN_TOTAL_AMOUNT,
+  // MINT_DECIMAL
+  // )({
+  // feePayer: feePayer.pubkey.toPubKey()
+  // });
+  // assert.isTrue(res.isOk, res.unwrap());
+  // const after = (await Wallet.getBalance(feePayer.pubkey.toPubKey())).unwrap();
+  // console.log('# tokenKey: ', res.unwrap());
+  // assert.isTrue(before > after, `before fee: ${before}, after fee: ${after}`);
+  // });
+
+  // it('Subscribe a account(pubkey)', async () => {
+  // const subscribeId = SplToken.subscribeAccount(
+  // dest.pubkey.toPubKey(),
+  // (v: SplToken.TransferHistory) => {
+  // console.log('# Subscribe result: ', v);
+  // assert.isNotEmpty(v.type);
+  // assert.isNotNull(v.date);
+  // assert.isNotNull(v.info.mint);
+  // assert.isNotEmpty(v.info.source);
+  // assert.isNotEmpty(v.info.destination);
+  // }
+  // );
+  // for (let i = 0; i < 3; i++) await sendContinuously();
+  // await Util.sleep(15);
+  // SplToken.unsubscribeAccount(subscribeId);
+  // assert.ok('success subscribe');
+  // });
+})
+
+const sendContinuously = async (): Promise<void> => {
+  const inst = await SplToken.transfer(
+    tokenKeyStr.toPubKey(),
+    source.pubkey.toPubKey(),
+    dest.pubkey.toPubKey(),
+    [source.secret.toKeypair()],
+    1,
+    MINT_DECIMAL
+  );
+  inst.isOk && inst.value.submit();
+}
 
 
