ade94bf33d7a554dc27a93e04f01f6ba423d0df8
fukaoi <goodakira7@gmail.com>
2021-12-07 15:48:20 +0900

    wip

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    index: "./src/index.ts"
+  },
+  output: {
+    path: __dirname + "/dist/",
+    filename: "solana-suite.js",
+    library: "solana-suite",
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+};
