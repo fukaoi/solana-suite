d9d3b6b785722ddf2ba4e09a7c8a86be890de07e
fukaoi <goodakira7@gmail.com>
2021-12-08 12:31:54 +0900

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: __dirname + "/dist/",
     filename: "solana-suite.js",
-    library: "solana-suite",
+    library: "SolanaSuite",
     libraryTarget: 'umd'
   },
   resolve: {
