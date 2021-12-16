ce6ed1535f57a0a541a220486ff61b977bb4ee7c
fukaoi <goodakira7@gmail.com>
2021-12-16 15:28:09 +0900

    wip

--- dist/cjs/src/sol-native.d.ts
+++
@@ -1,7 +0,0 @@
-import { PublicKey, Signer } from '@solana/web3.js';
-import { Result } from './';
-import { Instruction } from './instruction';
-export declare namespace SolNative {
-    const transferWithMultisig: (owner: PublicKey, dest: PublicKey, signers: Signer[], amountSol: number, feePayer?: Signer | undefined) => Promise<Result<Instruction, Error>>;
-    const transfer: (source: PublicKey, destination: PublicKey, signers: Signer[], amountSol: number, feePayer?: Signer | undefined) => Promise<Result<Instruction, Error>>;
-}
