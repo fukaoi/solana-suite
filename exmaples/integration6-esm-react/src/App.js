bec213ad8fab56afd8a7435800873c004f9b4049
fukaoi <goodakira7@gmail.com>
2022-01-11 11:21:03 +0900

    wip

--- exmaples/integration6-esm-react/src/App.js
+++ exmaples/integration6-esm-react/src/App.js
@@ -1,26 +1,10 @@
-import {Account} from 'solana-suite/packages/core';
+import {Account} from '@solana-suite/core';
 
-console.log(Account.create());
-// const wallet = AccountResotre.create('5385a4e4-039c-456d-a23f-d0e8f4b0850c','123456');
-// console.log(wallet);
-
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Integration6 example
+      `${Account.create()}`
     </div>
   );
 }
