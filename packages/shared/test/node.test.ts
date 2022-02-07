f97073cafeada2a759473133f758460261ca11ef
fukaoi <goodakira7@gmail.com>
2022-02-07 11:16:35 +0900

    update

--- packages/shared/test/node.test.ts
+++ packages/shared/test/node.test.ts
@@ -23,15 +23,15 @@ describe('Node', () => {
   });
 
   it('Connect devnet for localhost', async () => {
-    const res = Node.getConnection(Constants.SolanaNet.localhostDev);
+    const res = Node.getConnection(Constants.SolanaNet.localhost);
     assert.isNotEmpty(res);
-    assert.propertyVal(res, '_rpcEndpoint', ConstantsFunc.switchApi(Constants.SolanaNet.localhostDev));
+    assert.propertyVal(res, '_rpcEndpoint', ConstantsFunc.switchApi(Constants.SolanaNet.localhost));
   });
 
   it('Connect devnet for localhost by default no parameter', async () => {
     const res = Node.getConnection();
     assert.isNotEmpty(res);
-    assert.propertyVal(res, '_rpcEndpoint', ConstantsFunc.switchApi(Constants.SolanaNet.localhostDev));
+    assert.propertyVal(res, '_rpcEndpoint', ConstantsFunc.switchApi(Constants.SolanaNet.localhost));
   });
 
   it('Connect devnet and modified commitment', async () => {
