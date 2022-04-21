5518739309c6975c79bc452f3eee037bdc1522ac
fukaoi <goodakira7@gmail.com>
2022-04-21 19:46:40 +0900

    Modified debug console

---
+++ exmaples/integration7-transfer-to-users.ts
@@ -0,0 +1,116 @@
+//////////////////////////////////////////////
+// $ npx ts-node exmaples/integration7-transfer-to-users
+//////////////////////////////////////////////
+
+
+import assert from 'assert';
+import {
+  Account,
+  KeypairStr,
+  Pubkey,
+  SplToken,
+} from '@solana-suite/core';
+
+import {Node, sleep} from '@solana-suite/shared';
+
+const USERS_COUNT = 50;
+const SLEEP_TIME_WAIT = 1;
+
+(async () => {
+
+  Node.changeConnection({commitment: 'finalized'});
+
+  //////////////////////////////////////////////
+  // CREATE WALLET 
+  //////////////////////////////////////////////
+
+  let users = [];
+  for (let i = 0; i < USERS_COUNT; i++) {
+    users.push(Account.create());
+  }
+
+
+  // manual setting
+  // const owner = new KeypairStr(
+  // '',
+  // ''
+  // );
+
+  // random create
+  const owner = Account.create();
+  await Account.requestAirdrop(owner.toPublicKey());
+  console.log('# owner: ', owner.pubkey);
+
+  //////////////////////////////////////////////
+  // CREATE TOKEN 
+  //////////////////////////////////////////////
+
+  const totalAmount = 100000;
+  const decimals = 1;
+  const inst1 = await SplToken.mint(
+    owner.toPublicKey(),
+    [owner.toKeypair()],
+    totalAmount,
+    decimals
+  );
+
+  // submit instructions
+  (await inst1.submit()).match(
+    (value) => console.log('# mint nft sig: ', value),
+    (error) => assert(error)
+  );
+
+  const tokenKey = inst1.unwrap().data as Pubkey;
+  console.log('# tokenKey: ', tokenKey);
+
+  //////////////////////////////////////////////
+  // TRANSFER RECEIPR USER FROM THIS LINE 
+  //////////////////////////////////////////////
+
+  // ============== async ==============
+  // users.forEach(async (user) => {
+  // const inst2 = await SplToken.transfer(
+  // tokenKey.toPublicKey(),
+  // owner.toPublicKey(),
+  // user.toPublicKey(),
+  // [
+  // owner.toKeypair()
+  // ],
+  // 10,
+  // decimals
+  // );
+
+  // (await inst2.submit()).match(
+  // (value) => console.log('# Transfer nft sig: ', value),
+  // (error) => {assert(error); return;}
+  // );
+  // });
+
+  // ============== sync ============== 
+  let i = 1;
+  for (const user of users) {
+    await sleep(SLEEP_TIME_WAIT);
+    const inst2 = await SplToken.transfer(
+      tokenKey.toPublicKey(),
+      owner.toPublicKey(),
+      user.toPublicKey(),
+      [
+        owner.toKeypair()
+      ],
+      10,
+      decimals
+    );
+
+    (await inst2.submit()).match(
+      (value) => {
+        console.log('# Transfer nft sig: ', value);
+        console.log('# count: ', i);
+        i++;
+      },
+      (error) => {
+        assert(error);
+        return;
+      }
+    );
+  };
+})();
