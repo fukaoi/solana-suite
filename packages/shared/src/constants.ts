ea5ce5570b3eff1293252c6b41621b757fc52f8f
fukaoi <goodakira7@gmail.com>
2022-04-22 21:58:36 +0900

    modified

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -18,6 +18,7 @@ export namespace Constants {
 
   export const currentCluster = Config.cluster;
   export const isDebugging = Config.debugging;
+  export const nftstorageApikey = Config.nftstorage.apikey;
 }
 
 export namespace ConstantsFunc {
@@ -45,22 +46,6 @@ export namespace ConstantsFunc {
         return 'http://api.devnet.solana.com';
     }
   }
-
-  export const getNftStorageApiKey = () => {
-    if (!Config.nftstorage.apikey) {
-      console.warn(
-        `
-        [Warning]
-        --------------------------------------
-        If will use @solana-suite/nft package
-        your need to update nftstorage.apikey defin parameter in solana-suite.json.
-        can get apikey from https://nft.storage/
-        --------------------------------------
-        `
-      );
-    }
-    return Config.nftstorage.apikey;
-  }
 }
 
 export namespace Constants {
@@ -71,8 +56,7 @@ export namespace Constants {
   export const MEMO_PROGRAM_ID = 'Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo'.toPublicKey();
   export const METAPLEX_PROGRAM_ID = 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s'.toPublicKey();
   export const COMMITMENT: Commitment = 'confirmed';
-  // NFT.storage can store NFTs up to 32GB in size!
-  export const NFT_STORAGE_API_KEY = ConstantsFunc.getNftStorageApiKey() || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweERGMjcyN2VkODZhRGU1RTMyZDZDZEJlODc0YzRFNDlEODY1OWZmOEMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYyMDI2NDk0MzcwNiwibmFtZSI6ImRlbW8ifQ.d4J70mikxRB8a5vwNu6SO5HDA8JaueuseAj7Q_ytMCE';
+  export const NFT_STORAGE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweERGMjcyN2VkODZhRGU1RTMyZDZDZEJlODc0YzRFNDlEODY1OWZmOEMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYyMDI2NDk0MzcwNiwibmFtZSI6ImRlbW8ifQ.d4J70mikxRB8a5vwNu6SO5HDA8JaueuseAj7Q_ytMCE';
 
   export const NFT_STORAGE_GATEWAY_URL = 'https://ipfs.io/ipfs';
   export const ARWEAVE_UPLOAD_SRV_URL = 'https://us-central1-principal-lane-200702.cloudfunctions.net/uploadFile4';
