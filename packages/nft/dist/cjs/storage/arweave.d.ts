f1238b92d720b669a08ab01ceb42b6b459b91a19
fukaoi <goodakira7@gmail.com>
2022-06-29 15:58:47 +0900

    Start

--- packages/nft/dist/cjs/storage/arweave.d.ts
+++
@@ -1,6 +0,0 @@
-import { Storage } from './';
-import { Keypair } from '@solana/web3.js';
-import { Result } from '@solana-suite/shared';
-export declare namespace StorageArweave {
-    const upload: (payer: Keypair, storageData: Storage.Format) => Promise<Result<string, Error>>;
-}
