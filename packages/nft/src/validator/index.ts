73d639a206d74d01fc88c27732bf6c480d8cc585
fukaoi <goodakira7@gmail.com>
2022-08-17 09:56:47 +0900

    wip

--- packages/nft/src/validator/index.ts
+++ packages/nft/src/validator/index.ts
@@ -20,7 +20,7 @@ export namespace Validator {
   export const isName = (actual: string): Result<string, Error> => {
     if (actual.length < 0) {
       return Result.err(Error(Message.SMALL));
-    } else if (actual.length > 100) {
+    } else if (actual.length > 10) {
       return Result.err(Error(Message.BIG));
     }
     return Result.ok(Message.SUCCESS);
@@ -28,9 +28,20 @@ export namespace Validator {
 
   export const checkAll = (actualData: NftStorageMetadata) => {
     const keys = Object.keys(actualData);
+    let results: Array<{key: string, error: string}> = [];
     keys.map(key => {
-      console.log(key);
-      console.log(actualData[key]);
+      switch (key) {
+        case 'name':
+          const res = isName(actualData[key] as string);
+          if (res.isErr) {
+            results.push({ key, error: res.error.message });
+          }
+          break;
+        case 'royalty':
+          // results.push(isRoyalty(actualData[key] as number));
+          break;
+     }
     });
-  };
+    return Result.err(results as any);
+  }
 }
