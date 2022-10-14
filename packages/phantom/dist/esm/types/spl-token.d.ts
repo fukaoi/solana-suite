d5d6c83f14a17338143fa14472fe3a87dfa0870d
fukaoi <fukaoi@users.noreply.github.com>
2022-10-14 14:41:04 +0000

    Exec auto commit

--- packages/phantom/dist/esm/types/spl-token.d.ts
+++ packages/phantom/dist/esm/types/spl-token.d.ts
@@ -1,4 +1,4 @@
-import { Keypair, Transaction } from "@solana/web3.js";
+import { Keypair, Transaction } from '@solana/web3.js';
 export declare type InitializeMint = {
     mint: Keypair;
     tx: Transaction;
