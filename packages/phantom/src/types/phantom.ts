a4a6ecf022ddb9394642ef808a57a53d7929f82b
fukaoi <usapyon2022@gmail.com>
2022-10-24 21:54:50 +0900

    added typed for phantom wallet api

--- packages/phantom/src/types/phantom.ts
+++ packages/phantom/src/types/phantom.ts
@@ -5,6 +5,10 @@ import {
   PublicKey,
 } from '@solana/web3.js';
 
+export type connectOption = {
+   onlyIfTrusted: false
+}
+
 export type Phantom = {
   isPhantom?: boolean;
   publicKey: PublicKey;
@@ -16,7 +20,9 @@ export type Phantom = {
     options?: SendOptions
   ): Promise<{ signature: TransactionSignature }>;
   signMessage(message: Uint8Array): Promise<Uint8Array>;
-  connect(): Promise<void>;
+  connect(arg0: connectOption): Promise<{
+    publicKey: Uint16Array;
+  }>;
   disconnect(): Promise<void>;
   _handleDisconnect(...args: unknown[]): unknown;
 };
