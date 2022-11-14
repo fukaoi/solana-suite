54cc5805f36e6715e1636c8fdd54892ce71b23b2
fukaoi <usapyon2022@gmail.com>
2022-11-14 17:00:42 +0900

    Refactoring: functional partitioning

--- packages/core/src/sol-native/transfer.ts
+++ packages/core/src/sol-native/transfer.ts
@@ -1,87 +1,8 @@
-import {
-  createWrappedNativeAccount,
-  createMint,
-  createTransferInstruction,
-  createCloseAccountInstruction,
-} from '@solana/spl-token';
-
-import {
-  PublicKey,
-  SystemProgram,
-  Keypair,
-} from '@solana/web3.js';
-
-import {
-  Result,
-  Node,
-  Instruction,
-  debugLog,
-  Try,
-} from '@solana-suite/shared';
-
-import { AssociatedAccount } from '../associated-account';
+import { PublicKey, SystemProgram, Keypair } from '@solana/web3.js';
+import { Result, Instruction, Try } from '@solana-suite/shared';
 
 export namespace SolNative {
   const RADIX = 10;
-
-  // NOTICE: There is a lamports fluctuation when transfer under 0.001 sol
-  // for multiSig only function
-  export const transferWithMultisig = async (
-    owner: PublicKey,
-    dest: PublicKey,
-    signers: Keypair[],
-    amount: number,
-    feePayer?: Keypair
-  ): Promise<Result<Instruction, Error>> => {
-    return Try(async () => {
-      const connection = Node.getConnection();
-      const payer = feePayer ? feePayer : signers[0];
-      const wrapped = await createWrappedNativeAccount(
-        connection,
-        payer,
-        owner,
-        parseInt(`${amount.toLamports()}`, RADIX)
-      );
-
-      debugLog('# wrapped sol: ', wrapped.toBase58());
-
-      const token = await createMint(connection, payer, owner, owner, 0);
-
-      const sourceToken = await AssociatedAccount.retryGetOrCreate(
-        token,
-        owner,
-        payer
-      );
-
-      debugLog('# sourceToken: ', sourceToken);
-
-      const destToken = await AssociatedAccount.retryGetOrCreate(
-        token,
-        wrapped,
-        payer
-      );
-
-      debugLog('# destToken: ', destToken);
-
-      const inst1 = createTransferInstruction(
-        sourceToken.toPublicKey(),
-        destToken.toPublicKey(),
-        owner,
-        parseInt(`${amount}`, RADIX), // No lamports, its sol
-        signers
-      );
-
-      const inst2 = createCloseAccountInstruction(
-        wrapped,
-        dest,
-        owner,
-        signers
-      );
-
-      return new Instruction([inst1, inst2], signers, feePayer);
-    });
-  };
-
   export const transfer = (
     source: PublicKey,
     destination: PublicKey,
--- packages/core/test/sol-native/transfer.test.ts
+++ packages/core/test/sol-native/transfer.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, before } from 'mocha';
-import { SolNative, Multisig, KeypairStr, Airdrop } from '../../src';
+import { SolNative, KeypairStr, Airdrop } from '../../src';
 import { assert } from 'chai';
 import { Setup } from '../../../shared/test/testSetup';
 
@@ -58,38 +58,6 @@ describe('SolNative', () => {
     assert.isTrue(
       before.sol > after.sol,
       `before fee: ${before}, after fee: ${after}`
-    );
-  });
-
-  it('transfer transaction with multi sig', async () => {
-    const signer1 = KeypairStr.create();
-    const signer2 = KeypairStr.create();
-    const inst1 = await Multisig.create(2, source.toKeypair(), [
-      signer1.toPublicKey(),
-      signer2.toPublicKey(),
-    ]);
-
-    let multisig!: string;
-
-    (await inst1.submit()).match(
-      (_) => {
-        multisig = inst1.unwrap().data as string;
-        console.log('# multisig: ', multisig);
-      },
-      (err) => assert.fail(err.message)
-    );
-
-    const inst2 = await SolNative.transferWithMultisig(
-      multisig.toPublicKey(),
-      dest.toPublicKey(),
-      [source.toKeypair(), signer1.toKeypair(), signer2.toKeypair()],
-      0.01,
-      source.toKeypair()
-    );
-
-    (await inst2.submit()).match(
-      (sig) => console.log('# signature: ', sig),
-      (err) => assert.fail(err.message)
     );
   });
 });
