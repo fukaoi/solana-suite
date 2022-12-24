ef6071a9695ad77c7c1d06249cb7cee410e2f89b
fukaoi <usapyon2022@gmail.com>
2022-12-24 19:04:54 +0900

    fix: missed

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -47,7 +47,7 @@ export namespace Constants {
       case Constants.Cluster.prdMetaplex:
         return Constants.EndPointUrl.prdMetaplex;
       case Constants.Cluster.test:
-        return EndPointUrl.test;
+        return Constants.EndPointUrl.test;
       case Constants.Cluster.dev:
         return Constants.EndPointUrl.dev;
       default:
