6ebac61bd4cc98c62bb0e1318958d874ec854015
fukaoi <usapyon2022@gmail.com>
2023-01-27 15:05:05 +0900

    modified

--- examples/integration4-multisig.ts
+++ examples/integration4-multisig.ts
@@ -3,19 +3,10 @@
 //////////////////////////////////////////////
 
 import assert from 'assert';
-import {
-  KeypairStr,
-  Multisig,
-  SolNative,
-  SplToken,
-  Pubkey,
-  Airdrop,
-} from '@solana-suite/core';
+import { KeypairStr, Multisig, Pubkey, Airdrop } from '@solana-suite/core';
 
 import { Node } from '@solana-suite/shared';
 import { requestTransferByKeypair } from './requestTransferByKeypair';
-import { RandomAsset } from '@solana-suite/storage/test/randomAsset';
-import { StorageType } from '@solana-suite/shared-metaplex';
 
 (async () => {
   //////////////////////////////////////////////
@@ -28,9 +19,7 @@ import { StorageType } from '@solana-suite/shared-metaplex';
   // publisher: mint token, transfer token and pay transaction fee account
   // receipt: generally user account. sol not stocked
 
-  const owner = KeypairStr.create();
   const feePayer = KeypairStr.create();
-  const receipt = KeypairStr.create();
 
   // signer for multisig
   const signer1 = KeypairStr.create();
@@ -39,14 +28,12 @@ import { StorageType } from '@solana-suite/shared-metaplex';
 
   // faucet
   if (process.env.AIR_DROP) {
-    await Airdrop.request(owner.toPublicKey());
+    await Airdrop.request(feePayer.toPublicKey());
   } else {
-    await requestTransferByKeypair(owner.toPublicKey(), 0.6);
+    await requestTransferByKeypair(feePayer.toPublicKey(), 0.6);
   }
 
-  console.log('# owner: ', owner.pubkey);
-  console.log('# feePayer: ', receipt.pubkey);
-  console.log('# receipt: ', receipt.pubkey);
+  console.log('# feePayer: ', feePayer.pubkey);
   console.log('# signer1: ', signer1.pubkey);
   console.log('# signer2: ', signer2.pubkey);
   console.log('# signer3: ', signer3.pubkey);
@@ -54,80 +41,19 @@ import { StorageType } from '@solana-suite/shared-metaplex';
   //////////////////////////////////////////////
   // Setting multisig 2 of 2(m of n)
   //////////////////////////////////////////////
-  const signerPubkey = [
+  const signerPubkeys = [
     signer1.toPublicKey(),
     signer2.toPublicKey(),
     signer3.toPublicKey(),
   ];
 
-  const inst1 = await Multisig.create(2, owner.toKeypair(), signerPubkey);
-
-  //////////////////////////////////////////////
-  // TRANSFER FROM OWNER TO PUBLISHER
-  //////////////////////////////////////////////
-  const inst2 = SolNative.transfer(
-    owner.toPublicKey(), // from
-    feePayer.toPublicKey(), // to
-    [owner.toKeypair()], // signing
-    0.5 // 0.5 SOL
-  );
-
-  const publisher = inst1.unwrap().data as Pubkey;
-  console.log('# multisig address: ', publisher);
-
-  // submit batch instructions
-  await (
-    await [inst1, inst2].submit()
-  ).match(
+  const inst1 = await Multisig.create(2, feePayer.toKeypair(), signerPubkeys);
+  const multisig = inst1.unwrap().data as Pubkey;
+  (await inst1.submit()).match(
     async (value) => {
-      // [optional]if need this action. wait confirmation state
+      console.log('# multisig address: ', multisig);
       await Node.confirmedSig(value);
     },
     (error) => assert.fail(error)
-  );
-
-  //////////////////////////////////////////////
-  // CREATE SPL TOKEN
-  //////////////////////////////////////////////
-
-  const multiSigners = [signer1.toKeypair(), signer2.toKeypair()];
-  const tokenMetadata = {
-    name: 'solana-suite-token',
-    symbol: 'SST',
-    royalty: 50,
-    filePath: RandomAsset.get().filePath as string,
-    storageType: 'nftStorage' as StorageType,
-    isMutable: false,
-  };
-
-  // created by publisher account
-  const inst3 = await SplToken.mint(
-    publisher.toPublicKey(), // creator account
-    multiSigners, // signing
-    100000, // Total number of tokens issued
-    2, // token's decimal e.g:0.12, 20.52
-    tokenMetadata, // token standard metadata
-    feePayer.toKeypair() // pay transaction fee
-  );
-
-  const mint = inst3.unwrap().data as Pubkey;
-  console.log('# mint: ', mint);
-
-  const inst4 = await SplToken.transfer(
-    mint.toPublicKey(), // tokenKey
-    publisher.toPublicKey(), // from. own token
-    receipt.toPublicKey(), // to
-    multiSigners, // sinning
-    5000, // transfer amount
-    2, // token's decimal e.g:0.12, 20.52
-    feePayer.toKeypair() // pay transaction fee
-  );
-
-  // submit batch instructions
-  await (
-    await [inst3, inst4].submit()
-  ).match(
-    (value) => console.log('# sig url: ', value.toExplorerUrl()),
-    (error) => assert.fail(error.message)
   );
 })();
