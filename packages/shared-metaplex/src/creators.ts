610ac536cbe150274183393089b97891f30e9a09
fukaoi <usapyon2022@gmail.com>
2023-03-03 01:04:30 +0900

    modified

--- packages/shared-metaplex/src/creators.ts
+++ packages/shared-metaplex/src/creators.ts
@@ -2,11 +2,25 @@ import { CreatorInput, Creator } from '@metaplex-foundation/js';
 import { InputCreators, OutputCreators } from './types';
 
 export module Creators {
-  export const toInputConvert = (input: InputCreators): CreatorInput[] => {
-    return [];
+  export const toInputConvert = (input: InputCreators[]): CreatorInput[] => {
+    return input.map((data) => {
+      const modify: CreatorInput = {
+        address: data.address.toPublicKey(),
+        share: data.share,
+        authority: data.authority,
+      };
+      return modify;
+    });
   };
-  export const toOutputConvert = (output: Creator): OutputCreators[] => {
-    return [];
+
+  export const toOutputConvert = (output: Creator[]): OutputCreators[] => {
+    return output.map((data) => {
+      const modify: OutputCreators = {
+        address: data.address.toString(),
+        share: data.share,
+        verified: data.verified,
+      };
+      return modify;
+    });
   };
 }
-
