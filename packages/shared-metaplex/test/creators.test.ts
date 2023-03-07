b56b47bd1b938013250cbd4635776a09373cf797
fukaoi <usapyon2022@gmail.com>
2023-03-07 18:18:33 +0900

    plus test pattern

--- packages/shared-metaplex/test/creators.test.ts
+++ packages/shared-metaplex/test/creators.test.ts
@@ -7,25 +7,37 @@ describe('Creators', () => {
   it('To input convert', async () => {
     const creator1 = KeypairAccount.create();
     const creator2 = KeypairAccount.create();
+    const creator3 = KeypairAccount.create();
+    const creator4 = KeypairAccount.create();
     const input = [
       {
         address: creator1.pubkey,
-        share: 40,
+        share: 20,
         authority: creator1.secret,
       },
       {
         address: creator2.pubkey,
-        share: 60,
+        share: 30,
         authority: creator2.secret,
       },
+      {
+        address: creator3.pubkey,
+        share: 40,
+        authority: creator3.secret,
+      },
+      {
+        address: creator4.pubkey,
+        share: 10,
+        authority: creator4.secret,
+      },
     ];
-    const res = Creators.toInputConvert(input);
-    assert.equal(res[0].address.toString(), input[0].address);
-    assert.equal(res[0].share, input[0].share);
-    assert.deepEqual(res[0].authority, input[0].authority.toKeypair());
-    assert.equal(res[1].address.toString(), input[1].address);
-    assert.equal(res[1].share, input[1].share);
-    assert.deepEqual(res[1].authority, input[1].authority.toKeypair());
+    const results = Creators.toInputConvert(input);
+    console.log('# converted creators', results);
+    results.forEach((res, i) => {
+      assert.equal(res.address.toString(), input[i].address);
+      assert.equal(res.share, input[i].share);
+      assert.deepEqual(res.authority, input[i].authority.toKeypair());
+    });
   });
 
   it('To undefine convert', async () => {
