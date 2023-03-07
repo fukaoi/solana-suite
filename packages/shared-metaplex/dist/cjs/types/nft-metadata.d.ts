0ad07e52b555084aef25bc30d90736aa3f9a3fc5
fukaoi <usapyon2022@gmail.com>
2023-03-08 01:09:16 +0900

    reverted collection

--- packages/shared-metaplex/dist/cjs/types/nft-metadata.d.ts
+++ packages/shared-metaplex/dist/cjs/types/nft-metadata.d.ts
@@ -1,74 +1,92 @@
-import { CreateNftInput, MetaplexFileContent, BigNumber, Option, CreatorInput } from '@metaplex-foundation/js';
+import {
+  CreateNftInput,
+  MetaplexFileContent,
+  BigNumber,
+  Option,
+  CreatorInput,
+} from '@metaplex-foundation/js';
 import { Uses } from '@metaplex-foundation/mpl-token-metadata';
 import { Pubkey, Secret } from '@solana-suite/shared';
 import { StorageType } from './nft-storage-metadata';
-type noNeedOptional = 'payer' | 'owner' | 'associatedTokenProgram' | 'tokenProgram' | 'confirmOptions';
+type noNeedOptional =
+  | 'payer'
+  | 'owner'
+  | 'associatedTokenProgram'
+  | 'tokenProgram'
+  | 'confirmOptions';
 export type InputCreators = {
-    readonly address: Pubkey;
-    readonly share: number;
-    readonly authority?: Secret | undefined;
+  readonly address: Pubkey;
+  readonly share: number;
+  readonly authority?: Secret | undefined;
 };
 export type OutputCreators = {
-    readonly address: Pubkey;
-    readonly share: number;
-    readonly verified: boolean;
+  readonly address: Pubkey;
+  readonly share: number;
+  readonly verified: boolean;
 };
 export type JsonMetadataAttribute = {
-    trait_type?: string;
-    value?: string;
-    [key: string]: unknown;
+  trait_type?: string;
+  value?: string;
+  [key: string]: unknown;
 };
 export type JsonMetadataProperties = {
-    creators?: {
-        address?: string;
-        share?: number;
-        [key: string]: unknown;
-    }[];
-    files?: {
-        type?: string;
-        uri?: string;
-        [key: string]: unknown;
-    }[];
+  creators?: {
+    address?: string;
+    share?: number;
     [key: string]: unknown;
+  }[];
+  files?: {
+    type?: string;
+    uri?: string;
+    [key: string]: unknown;
+  }[];
+  [key: string]: unknown;
 };
 export type InputNftMetadata = {
-    name: string;
-    symbol: string;
-    royalty: number;
-    filePath: MetaplexFileContent;
-    storageType: StorageType;
-    description?: string;
-    external_url?: string;
-    attributes?: JsonMetadataAttribute[];
-    properties?: JsonMetadataProperties;
-    isMutable?: boolean;
-    maxSupply?: BigNumber;
-  //collections
-    creators?: InputCreators[];
-    uses?: Option<Uses>;
-    options?: {
-        [key: string]: unknown;
-    };
+  name: string;
+  symbol: string;
+  royalty: number;
+  filePath: MetaplexFileContent;
+  storageType: StorageType;
+  description?: string;
+  external_url?: string;
+  attributes?: JsonMetadataAttribute[];
+  properties?: JsonMetadataProperties;
+  isMutable?: boolean;
+  maxSupply?: BigNumber;
+  isCollection?: boolean;
+  collection?: Option<Pubkey>;
+  collectionAuthority?: Option<Secret>;
+  collectionAuthorityIsDelegated?: boolean;
+  collectionIsSized?: boolean;
+  creators?: InputCreators[];
+  uses?: Option<Uses>;
+  options?: {
+    [key: string]: unknown;
+  };
 };
 export type OutputNftMetadata = {
-    mint: string;
-    updateAuthority: string;
-    royalty: number;
-    name: string;
-    symbol: string;
-    uri: string;
-    isMutable: boolean;
-    primarySaleHappened: boolean;
-    creators: OutputCreators[];
-    editionNonce: Option<number>;
-    // collection: Option<{
-    //     address: Pubkey;
-    //     verified: boolean;
-    // }>;
-    uses: Option<Uses>;
+  mint: string;
+  updateAuthority: string;
+  royalty: number;
+  name: string;
+  symbol: string;
+  uri: string;
+  isMutable: boolean;
+  primarySaleHappened: boolean;
+  creators: OutputCreators[];
+  editionNonce: Option<number>;
+  // collection: Option<{
+  //     address: Pubkey;
+  //     verified: boolean;
+  // }>;
+  uses: Option<Uses>;
 };
 export type _MetaplexNftMetaData = Omit<CreateNftInput, noNeedOptional>;
-export type _InputNftMetadata = Omit<InputNftMetadata, 'creators'> & {
-    creators: CreatorInput[];
+export type _InputNftMetadata = Omit<
+  InputNftMetadata,
+  'creators' | 'collection' | 'collectionAuthority'
+> & {
+  creators: CreatorInput[];
 };
 export {};
