24500cff732683a189c8cdd1a116d141cc065129
fukaoi <usapyon2022@gmail.com>
2023-05-02 16:29:03 +0900

    updated

--- packages/nft/src/metaplex/find.ts
+++ packages/nft/src/metaplex/find.ts
@@ -12,66 +12,62 @@ import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import fetch from 'cross-fetch';
 
 // Sort by latest with unixtimestamp function
-export const sortDescByUinixTimestamp = (
-  a: UserSideOutput.NftMetadata | UserSideOutput.TokenMetadata,
-  b: UserSideOutput.NftMetadata | UserSideOutput.TokenMetadata
-): number => {
-  if (!a.offchain.created_at) {
-    a.offchain.created_at = 0;
-  }
-  if (!b.offchain.created_at) {
-    b.offchain.created_at = 0;
-  }
-  return b.offchain.created_at - a.offchain.created_at;
-};
-
-export const sortAscByUinixTimestamp = (
-  a: UserSideOutput.NftMetadata | UserSideOutput.TokenMetadata,
-  b: UserSideOutput.NftMetadata | UserSideOutput.TokenMetadata
-): number => {
-  if (!a.offchain.created_at) {
-    a.offchain.created_at = 0;
-  }
-  if (!b.offchain.created_at) {
-    b.offchain.created_at = 0;
-  }
-  return a.offchain.created_at - b.offchain.created_at;
-};
+export const sortByUinixTimestamp =
+  <T extends UserSideOutput.NftMetadata | UserSideOutput.TokenMetadata>(
+    sortable: Sortable
+  ) =>
+  (a: T, b: T): number => {
+    if (!a.offchain.created_at) {
+      a.offchain.created_at = 0;
+    }
+    if (!b.offchain.created_at) {
+      b.offchain.created_at = 0;
+    }
+    if (sortable === Sortable.Desc) {
+      return b.offchain.created_at - a.offchain.created_at;
+    } else if (sortable === Sortable.Asc) {
+      return a.offchain.created_at - b.offchain.created_at;
+    } else {
+      throw Error(`No match sortable: ${sortable}`);
+    }
+  };
 
 export enum Sortable {
   Asc = 'asc',
   Desc = 'desc',
 }
 
-export type FindByOwnerCallback = (result: Result<[], Error>) => void;
+export type FindByOwnerCallback = <T>(result: Result<T[], Error>) => void;
 
-export const converter = (
+export const converter = <T>(
   tokenStandard: UserSideInput.TokenStandard,
   metadata: Metadata,
   json: InfraSideOutput.Offchain
-): UserSideOutput.NftMetadata | UserSideOutput.TokenMetadata => {
+): T => {
   if (tokenStandard === UserSideInput.TokenStandard.Fungible) {
     return Convert.TokenMetadata.intoUserSide({
       onchain: metadata,
       offchain: json,
-    });
+    }) as T;
   } else if (tokenStandard === UserSideInput.TokenStandard.NonFungible) {
     return Convert.NftMetadata.intoUserSide({
       onchain: metadata,
       offchain: json,
-    });
+    }) as T;
   } else {
     throw Error(`No match tokenStandard: ${tokenStandard}`);
   }
 };
 
-export const abstractFindByOwner = async (
+export const genericFindByOwner = async <
+  T extends UserSideOutput.NftMetadata | UserSideOutput.TokenMetadata
+>(
   owner: Pubkey,
   callback: FindByOwnerCallback,
   tokenStandard: UserSideInput.TokenStandard,
   sortable?: Sortable
 ): Promise<void> => {
-  let datas: UserSideOutput.TokenMetadata[] | UserSideOutput.NftMetadata[] = [];
+  let datas: T[] = [];
   try {
     const connection = Node.getConnection();
     const info = await connection.getParsedTokenAccountsByOwner(
@@ -82,37 +78,35 @@ export const abstractFindByOwner = async (
     );
 
     for await (const d of info.value) {
-      if (d.account.data.parsed.info.tokenAmount.uiAmount == 1) {
-        const mint = d.account.data.parsed.info.mint;
-        const metadata = await Metadata.fromAccountAddress(
-          connection,
-          Pda.getMetadata(mint)
-        );
-        debugLog('# findByOwner metadata: ', metadata);
-
-        // tokenStandard: 0(NFT) or 2 (SPL-TOKEN)
-        if (metadata.tokenStandard !== tokenStandard) {
-          continue;
-        }
-        fetch(metadata.data.uri).then((response) => {
-          debugLog('# findByOwner response: ', metadata);
-          response
-            .json()
-            .then((json) => {
-              const modified = converter(tokenStandard, metadata, json);
-              datas.push(modified as UserSideOutput.NftMetadata);
-              if (sortable === Sortable.Desc) {
-                datas = datas.sort(sortDescByUinixTimestamp);
-              } else if (sortable === Sortable.Asc) {
-                datas = datas.sort(sortAscByUinixTimestamp);
-              }
-              callback(Result.ok(datas));
-            })
-            .catch((e) => {
-              callback(Result.err(e));
-            });
-        });
+      const mint = d.account.data.parsed.info.mint;
+      const metadata = await Metadata.fromAccountAddress(
+        connection,
+        Pda.getMetadata(mint)
+      );
+      debugLog('# findByOwner metadata: ', metadata);
+      // tokenStandard: 0(NFT) or 2 (SPL-TOKEN)
+      if (metadata.tokenStandard !== tokenStandard) {
+        continue;
       }
+      fetch(metadata.data.uri).then((response) => {
+        debugLog('# findByOwner response: ', metadata);
+        response
+          .json()
+          .then((json) => {
+            datas.push(converter(tokenStandard, metadata, json));
+            const descAlgo = sortByUinixTimestamp<T>(Sortable.Desc);
+            const ascAlgo = sortByUinixTimestamp<T>(Sortable.Asc);
+            if (sortable === Sortable.Desc) {
+              datas = datas.sort(descAlgo);
+            } else if (sortable === Sortable.Asc) {
+              datas = datas.sort(ascAlgo);
+            }
+            callback(Result.ok(datas));
+          })
+          .catch((e) => {
+            callback(Result.err(e));
+          });
+      });
     }
   } catch (e) {
     console.error('# retry: ', e);
@@ -136,56 +130,82 @@ export namespace Metaplex {
     callback: FindByOwnerCallback,
     sortable?: Sortable
   ): Promise<void> => {
-    let nftDatas: UserSideOutput.NftMetadata[] = [];
-    try {
-      const connection = Node.getConnection();
-      const info = await connection.getParsedTokenAccountsByOwner(
-        owner.toPublicKey(),
-        {
-          programId: TOKEN_PROGRAM_ID,
-        }
-      );
-
-      // tokenStandard: 0(NFT) or 2 (SPL-TOKEN)
-      for await (const d of info.value) {
-        if (d.account.data.parsed.info.tokenAmount.uiAmount == 1) {
-          const mint = d.account.data.parsed.info.mint;
-          const metadata = await Metadata.fromAccountAddress(
-            connection,
-            Pda.getMetadata(mint)
-          );
-          debugLog('# findByOwner metadata: ', metadata);
-          if (metadata.tokenStandard !== 0) {
-            continue;
-          }
-          fetch(metadata.data.uri).then((response) => {
-            debugLog('# findByOwner response: ', metadata);
-            response
-              .json()
-              .then((json) => {
-                const modified = Convert.NftMetadata.intoUserSide({
-                  onchain: metadata,
-                  offchain: json,
-                });
-                nftDatas.push(modified);
-                if (sortable === Sortable.Desc) {
-                  nftDatas = nftDatas.sort(sortDescByUinixTimestamp);
-                } else if (sortable === Sortable.Asc) {
-                  nftDatas = nftDatas.sort(sortAscByUinixTimestamp);
-                }
-                callback(Result.ok(nftDatas));
-              })
-              .catch((e) => {
-                callback(Result.err(e));
-              });
-          });
-        }
-      }
-    } catch (e) {
-      console.error('# retry: ', e);
-      if (e instanceof Error) {
-        callback(Result.err(e));
-      }
-    }
+    genericFindByOwner<UserSideOutput.NftMetadata>(
+      owner,
+      callback,
+      UserSideInput.TokenStandard.NonFungible,
+      sortable
+    );
   };
+
+  export const findByOwner2 = async (
+    owner: Pubkey,
+    callback: FindByOwnerCallback,
+    sortable?: Sortable
+  ): Promise<void> => {
+    genericFindByOwner<UserSideOutput.TokenMetadata>(
+      owner,
+      callback,
+      UserSideInput.TokenStandard.Fungible,
+      sortable
+    );
+  };
+
+  // export const findByOwner = async (
+  //   owner: Pubkey,
+  //   callback: FindByOwnerCallback,
+  //   sortable?: Sortable
+  // ): Promise<void> => {
+  //   let nftDatas: UserSideOutput.NftMetadata[] = [];
+  //   try {
+  //     const connection = Node.getConnection();
+  //     const info = await connection.getParsedTokenAccountsByOwner(
+  //       owner.toPublicKey(),
+  //       {
+  //         programId: TOKEN_PROGRAM_ID,
+  //       }
+  //     );
+  //
+  //     // tokenStandard: 0(NFT) or 2 (SPL-TOKEN)
+  //     for await (const d of info.value) {
+  //       if (d.account.data.parsed.info.tokenAmount.uiAmount == 1) {
+  //         const mint = d.account.data.parsed.info.mint;
+  //         const metadata = await Metadata.fromAccountAddress(
+  //           connection,
+  //           Pda.getMetadata(mint)
+  //         );
+  //         debugLog('# findByOwner metadata: ', metadata);
+  //         if (metadata.tokenStandard !== 0) {
+  //           continue;
+  //         }
+  //         fetch(metadata.data.uri).then((response) => {
+  //           debugLog('# findByOwner response: ', metadata);
+  //           response
+  //             .json()
+  //             .then((json) => {
+  //               const modified = Convert.NftMetadata.intoUserSide({
+  //                 onchain: metadata,
+  //                 offchain: json,
+  //               });
+  //               nftDatas.push(modified);
+  //               if (sortable === Sortable.Desc) {
+  //                 nftDatas = nftDatas.sort(sortDescByUinixTimestamp);
+  //               } else if (sortable === Sortable.Asc) {
+  //                 nftDatas = nftDatas.sort(sortAscByUinixTimestamp);
+  //               }
+  //               callback(Result.ok(nftDatas));
+  //             })
+  //             .catch((e) => {
+  //               callback(Result.err(e));
+  //             });
+  //         });
+  //       }
+  //     }
+  //   } catch (e) {
+  //     console.error('# retry: ', e);
+  //     if (e instanceof Error) {
+  //       callback(Result.err(e));
+  //     }
+  //   }
+  // };
 }
--- packages/nft/test/metaplex/find.test.ts
+++ packages/nft/test/metaplex/find.test.ts
@@ -1,78 +1,56 @@
 import { describe, it } from 'mocha';
 import { assert } from 'chai';
 import { Metaplex } from '../../src/metaplex';
-import { Node } from '@solana-suite/shared';
 import { Sortable } from '../../src/metaplex/find';
-import { UserSideOutput } from '@solana-suite/shared-metaplex';
 
 describe('Metaplex.find', () => {
-  it('Find owner info', async () => {
+  it.only('Find owner info', async () => {
     console.log('#Normal: ');
     const owner = 'CGDRajhcFo9ysuUjBsbwCQHKJuCHiXeEUrMKSot1eyay';
     await Metaplex.findByOwner(owner, (result) => {
       result.match(
         (ok) => {
           console.log(ok);
+          assert.isNotEmpty(ok);
         },
         (err) => assert.fail(err.message)
       );
     });
   });
 
-  // it('Find owner info with Desc', async () => {
-  //   console.log('#Desc: ');
-  //   // Node.changeConnection({
-  //   //   customClusterUrl: [
-  //   //     'https://sparkling-ancient-snow.solana-devnet.discover.quiknode.pro/caf0ca64ee225f376da0d05ef893c57a2b6644a7/',
-  //   //   ],
-  //   // });
-  //   // const owner = 'HAjRB3jEdBDcmqnXuZcwMNZUZ92XpkKjN4GTVuvKwsBK';
-  //   // const owner = 'BAWTL3RSxNe2mN7uS6MUvyWmBDBXUDQRNQftrS1R6baS';
-  //   const owner = 'CGDRajhcFo9ysuUjBsbwCQHKJuCHiXeEUrMKSot1eyay';
-  //   const res = await Metaplex.findByOwner(owner, Sortable.Desc);
-  //   console.log(res);
-  // });
+  it('Find owner info with Desc', async () => {
+    console.log('#Desc: ');
+    const owner = 'CGDRajhcFo9ysuUjBsbwCQHKJuCHiXeEUrMKSot1eyay';
+    await Metaplex.findByOwner(
+      owner,
+      (result) => {
+        result.match(
+          (ok) => {
+            console.log(ok);
+            assert.isNotEmpty(ok);
+          },
+          (err) => assert.fail(err.message)
+        );
+      },
+      Sortable.Desc
+    );
+  });
 
-  // it.only('Find owner info with Asc', async () => {
-  //   console.log('#Asc: ');
-  //   // Node.changeConnection({
-  //   //   customClusterUrl: [
-  //   //     'https://sparkling-ancient-snow.solana-devnet.discover.quiknode.pro/caf0ca64ee225f376da0d05ef893c57a2b6644a7/',
-  //   //   ],
-  //   // });
-  //   // const owner = 'HAjRB3jEdBDcmqnXuZcwMNZUZ92XpkKjN4GTVuvKwsBK';
-  //   // const owner = 'BAWTL3RSxNe2mN7uS6MUvyWmBDBXUDQRNQftrS1R6baS';
-  //   const owner = 'CGDRajhcFo9ysuUjBsbwCQHKJuCHiXeEUrMKSot1eyay';
-  //   const res = await Metaplex.findByOwner(owner, Sortable.Asc);
-  //   console.log(res);
-  // });
-
-  // it.skip('Find owner info', async () => {
-  //   // const owner = 'FbreoZcjxH4h8qfptQmGEGrwZLcPMbdHfoTJycAjtfu';
-  //   const owner = 'BAWTL3RSxNe2mN7uS6MUvyWmBDBXUDQRNQftrS1R6baS';
-  //   const res = await Metaplex.findByOwner(owner);
-  //   res.match(
-  //     (ok) => {
-  //       assert.isTrue(ok.length > 0);
-  //       console.log('# find owner info: ', ok);
-  //     },
-  //     (err) => {
-  //       assert.fail(err.message);
-  //     }
-  //   );
-  // });
-  //
-  // it.skip('Find owner info, many info', async () => {
-  //   const owner = '6yVHt5qgGnGZ3rGJoX9dX5zKpWgvmz5rLgzco77HiW2H';
-  //   const res = await Metaplex.findByOwner(owner);
-  //   res.match(
-  //     (ok) => {
-  //       assert.isTrue(ok.length > 0);
-  //       console.log('# find owner info: ', ok);
-  //     },
-  //     (err) => {
-  //       assert.fail(err.message);
-  //     }
-  //   );
-  // });
+  it('Find owner info with Asc', async () => {
+    console.log('#Asc: ');
+    const owner = 'CGDRajhcFo9ysuUjBsbwCQHKJuCHiXeEUrMKSot1eyay';
+    await Metaplex.findByOwner(
+      owner,
+      (result) => {
+        result.match(
+          (ok) => {
+            console.log(ok);
+            assert.isNotEmpty(ok);
+          },
+          (err) => assert.fail(err.message)
+        );
+      },
+      Sortable.Asc
+    );
+  });
 });
