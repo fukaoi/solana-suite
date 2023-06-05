33a2758d47ddbc4090a029d785b9e93ff0e2246d
fukaoi <fukaoi@users.noreply.github.com>
2023-06-05 07:20:39 +0000

    Exec auto commit

--- packages/core/dist/cjs/types/spl-token/find.d.ts
+++
@@ -1,8 +0,0 @@
-import { UserSideOutput } from '@solana-suite/shared-metaplex';
-export declare enum Sortable {
-    Asc = "asc",
-    Desc = "desc"
-}
-export type OnOk = (ok: UserSideOutput.TokenMetadata[]) => void;
-export type OnErr = (err: Error) => void;
-//# sourceMappingURL=find.d.ts.map
