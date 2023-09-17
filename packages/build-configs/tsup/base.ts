15bff6debc93c6fcefe799fdfe57f90faa40401c
fukaoi <usapyon2022@gmail.com>
2023-09-17 23:17:51 +0900

    wip

--- packages/build-configs/tsup/base.ts
+++ packages/build-configs/tsup/base.ts
@@ -4,7 +4,9 @@ const env = process.env.NODE_ENV;
 
 export default defineConfig({
   entry: {
-    index: 'src/index.ts',
+    // index: 'src/index.ts',
+    index: 'src/*',
+    // '~/': 'src/',
   },
   format: ['cjs', 'esm'],
   tsconfig: './tsconfig.json',
--- packages/shared/src/global.ts
+++ packages/shared/src/global.ts
@@ -1,13 +1,13 @@
 import bs from 'bs58';
 import { Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
-import { Constants } from './constants';
-import { Node } from './node';
-import { Result } from './result';
-import { Instruction as _Instruction } from './instruction';
-import { Instruction as _Batch } from './instruction/batch-submit';
-import { KeypairAccount } from './keypair-account';
+import { Constants } from '~/constants';
+import { Node } from '~/node';
+import { Result } from '~/result';
+import { Instruction as _Instruction } from '~/instruction';
+import { Instruction as _Batch } from '~/instruction/batch-submit';
+import { KeypairAccount } from '~/keypair-account';
 import { BigNumber } from 'bignumber.js';
-import { AnyObject, Explorer } from './types/global';
+import { AnyObject, Explorer } from '~/types/global';
 
 /**
  * senTransaction() TransactionInstruction
@@ -73,7 +73,7 @@ String.prototype.toKeypair = function () {
  * @returns string
  */
 String.prototype.toExplorerUrl = function (
-  explorer: Explorer = Explorer.Solscan
+  explorer: Explorer = Explorer.Solscan,
 ) {
   const endPointUrl = Node.getConnection().rpcEndpoint;
   debugLog('# toExplorerUrl rpcEndpoint:', endPointUrl);
@@ -149,7 +149,7 @@ export const overwriteObject = (
   targets: {
     existsKey: string;
     will: { key: string; value: unknown };
-  }[]
+  }[],
 ): unknown => {
   const that: AnyObject = object as AnyObject;
   targets.forEach((target) => {
@@ -172,7 +172,7 @@ export const debugLog = (
   data1: unknown,
   data2: unknown = '',
   data3: unknown = '',
-  data4: unknown = ''
+  data4: unknown = '',
 ): void => {
   if (Constants.isDebugging === 'true' || process.env.DEBUG === 'true') {
     console.log('[DEBUG]', data1, data2, data3, data4);
@@ -236,19 +236,19 @@ export const isPromise = (obj: unknown): obj is Promise<unknown> => {
  */
 export function Try<T, E extends Error>(
   asyncblock: () => Promise<T>,
-  finallyInput?: () => void
+  finallyInput?: () => void,
 ): Promise<Result<T, E>>;
 export function Try<T, E extends Error>(block: () => T): Result<T, E>;
 export function Try<T, E extends Error>(
   input: () => Promise<T>,
-  finallyInput?: () => void
+  finallyInput?: () => void,
 ): Result<T, Error> | Promise<Result<T, Error>> {
   try {
     const v = input();
     if (isPromise(v)) {
       return v.then(
         (x: T) => Result.ok(x),
-        (err: E) => Result.err(err)
+        (err: E) => Result.err(err),
       );
     } else {
       return Result.ok(v);
@@ -273,7 +273,7 @@ export function Try<T, E extends Error>(
  * @returns Date | undefined
  */
 export const convertTimestampToDateTime = (
-  created_at: number | undefined
+  created_at: number | undefined,
 ): Date | undefined => {
   if (created_at) {
     return new Date(created_at * 1000);
