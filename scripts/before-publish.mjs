5c8c3130909d3c9a0acb7ea4cb0b8e0da5ae05f3
fukaoi <usapyon2022@gmail.com>
2023-10-02 18:45:35 +0900

    updated

--- scripts/before-publish.mjs
+++ scripts/before-publish.mjs
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 
-import { readFileSync, renameSync, writeFileSync } from 'node:fs';
+import { readFileSync, renameSync, writeFileSync } from "node:fs";
 
-const CURRENT_PACKAGE_FILE = './package.json';
+const CURRENT_PACKAGE_FILE = "./package.json";
 
 (() => {
   try {
     const packageJson = readFileSync(CURRENT_PACKAGE_FILE).toString();
     renameSync(CURRENT_PACKAGE_FILE, `${CURRENT_PACKAGE_FILE}.org`);
     const parsed = JSON.parse(packageJson);
-    delete parsed.dependencies;
     delete parsed.devDependencies;
     writeFileSync(CURRENT_PACKAGE_FILE, JSON.stringify(parsed, null, 2));
   } catch (err) {
-    console.error('# before-publish error: ', err);
+    console.error("# before-publish error: ", err);
   }
 })();
