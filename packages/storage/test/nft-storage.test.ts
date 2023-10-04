4b455221a8c75a415cd2ff4e2ade95e9581c7870
fukaoi <usapyon2022@gmail.com>
2023-10-04 11:15:27 +0900

    Fi

--- packages/storage/test/nft-storage.test.ts
+++ packages/storage/test/nft-storage.test.ts
@@ -1,30 +1,27 @@
-import { describe, it } from 'mocha';
-import { assert } from 'chai';
-import { RandomAsset } from './randomAsset';
-import { NftStorage } from '../src/nft-storage';
+import test from 'ava';
+import { RandomAsset } from 'test-tools/setupAsset';
+import { NftStorage } from '../src/';
 
-describe('StorageNftStorage', () => {
-  it('Upload content data', async () => {
-    const asset = RandomAsset.get();
-    const res = await NftStorage.uploadContent(asset.filePath!);
+test('Upload content data', async (t) => {
+  const asset = RandomAsset.get();
+  const res = await NftStorage.uploadContent(asset.filePath!);
 
-    res.match(
-      (ok) => console.log('# nft.storage content url: ', ok),
-      (err) => assert.fail(err.message)
-    );
-  });
+  res.match(
+    (ok) => t.pass(`# nft.storage content url: ${ok}`),
+    (err) => t.fail(err.message),
+  );
+});
 
-  it('Upload metadata json', async () => {
-    const asset = RandomAsset.get();
-    const image = {
-      image: 'https://arweave.net/mVT6g3X99bZG0oMlTBB8fdbH7arnQ9lKWMUR9jMTXbQ',
-    };
-    const meta = { ...asset, ...image };
-    const res = await NftStorage.uploadMetadata(meta);
+test('Upload metadata json', async (t) => {
+  const asset = RandomAsset.get();
+  const image = {
+    image: 'https://arweave.net/mVT6g3X99bZG0oMlTBB8fdbH7arnQ9lKWMUR9jMTXbQ',
+  };
+  const meta = { ...asset, ...image };
+  const res = await NftStorage.uploadMetadata(meta);
 
-    res.match(
-      (ok) => console.log('# nft.storage metadata url: ', ok),
-      (err) => assert.fail(err.message)
-    );
-  });
+  res.match(
+    (ok) => t.pass(`# nft.storage content url: ${ok}`),
+    (err) => t.fail(err.message),
+  );
 });
