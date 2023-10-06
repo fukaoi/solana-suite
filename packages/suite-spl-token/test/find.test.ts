2d8aa5c22b23270dee3136494a08e1ecc16cd902
fukaoi <usapyon2022@gmail.com>
2023-10-06 22:09:08 +0900

    Fix test, eslint

--- packages/suite-spl-token/test/find.test.ts
+++ packages/suite-spl-token/test/find.test.ts
@@ -1,4 +1,4 @@
-import test, { ExecutionContext } from 'ava';
+import test from 'ava';
 import { Setup } from 'test-tools/setup';
 import { Pubkey } from '~/types/account';
 import { SplToken } from '../src/';
@@ -6,36 +6,66 @@ import { Sortable } from '~/types/find';
 import { OnErr, OnOk } from '~/types/shared';
 import { UserSideOutput } from '~/types/converter';
 import { promisify } from 'node:util';
+import { sleep } from '../../shared/src/shared';
 
 let owner: Pubkey;
 const nftMint = '5cjaV2QxSrZ3qESwsH49JmQqrcakThBZ9uZ5NVCcqzHt'; // nft
 const mint = 'EFgwtsm4azvQcnRPhDZ8yV9we1A12PgecpJ3im79o4x3'; // token
 const notFoundTokenOwner = '93MwWVSZHiPS9VLay4ywPcTWmT4twgN2nxdCgSx6uFT';
+
 test.before(async () => {
   const obj = await Setup.generateKeyPair();
   owner = obj.source.pubkey;
 });
 
-test.only('Not found token', async (t) => {
-  const onOk: OnOk<UserSideOutput.TokenMetadata> = (ok) => {
-    console.log('ok: ', ok);
-    // t.fail('Do not come here');
-  };
-  // const onErr: OnErr = (err) => t.is(err.message, 'fetch failed');
+/* eslint-disable */
+const withCallback = (fn: any) => async (t: any) => {
+  await promisify(fn)(t);
+  t.pass();
+};
 
-  const wrap = promisify((err: Error, pubkey: Pubkey, callback: any) => {
-    SplToken.findByOwner(pubkey, callback, console.log);
-  });
-  const res = await wrap(notFoundTokenOwner);
-  console.log(res);
-  t.true(res);
-});
+test(
+  'Not found token',
+  withCallback((t: any, end: any) => {
+    const onOk: OnOk<UserSideOutput.TokenMetadata> = (ok) => {
+      t.true(Array.isArray(ok));
+      end();
+    };
+    const onErr: OnErr = (err) => t.fail(err.message);
+    SplToken.findByOwner(notFoundTokenOwner, onOk, onErr);
+  }),
+);
 
-test('Get token info owned', (t) => {
-  const onErr: OnErr = (err: Error) => t.fail(err.message);
-  SplToken.findByOwner(
-    owner,
-    (ok: UserSideOutput.TokenMetadata[]) => {
+test(
+  'Get token info owned',
+  withCallback(async (t: any, end: any) => {
+    const onErr: OnErr = (err: Error) => t.fail(err.message);
+    SplToken.findByOwner(
+      owner,
+      (ok: UserSideOutput.TokenMetadata[]) => {
+        t.log(ok);
+        ok.forEach((res) => {
+          t.not(res.name, '');
+          t.not(res.mint, '');
+          t.not(res.symbol, '');
+          t.not(res.uri, '');
+          t.is(typeof res.royalty, 'number');
+          t.not(res.offchain, '');
+          t.not(res.tokenAmount, '');
+        });
+      },
+      onErr,
+    );
+    await sleep(2);
+    end();
+  }),
+);
+
+test(
+  'Get token info owned with no Hold',
+  withCallback(async (t: any, end: any) => {
+    const onOk: OnOk<UserSideOutput.TokenMetadata> = async (ok) => {
+      t.log(ok);
       ok.forEach((res) => {
         t.not(res.name, '');
         t.not(res.mint, '');
@@ -45,60 +75,56 @@ test('Get token info owned', (t) => {
         t.not(res.offchain, '');
         t.not(res.tokenAmount, '');
       });
-    },
-    onErr,
-  );
-});
+    };
+    const onErr: OnErr = (err: Error) => t.fail(err.message);
+    SplToken.findByOwner(owner, onOk, onErr, { isHolder: false });
+    await sleep(2);
+    end();
+  }),
+);
 
-test('Get token info owned with no Hold', (t) => {
-  const onOk: OnOk<UserSideOutput.TokenMetadata> = (ok) => {
-    ok.forEach((res) => {
-      t.not(res.name, '');
-      t.not(res.mint, '');
-      t.not(res.symbol, '');
-      t.not(res.uri, '');
-      t.is(typeof res.royalty, 'number');
-      t.not(res.offchain, '');
-      t.not(res.tokenAmount, '');
-    });
-  };
-  const onErr: OnErr = (err: Error) => t.fail(err.message);
+test(
+  'Get token info owned with Asc',
+  withCallback(async (t: any, end: any) => {
+    const onOk: OnOk<UserSideOutput.TokenMetadata> = (ok) => {
+      t.log(ok);
+      ok.forEach((res) => {
+        t.not(res.name, '');
+        t.not(res.mint, '');
+        t.not(res.symbol, '');
+        t.not(res.uri, '');
+        t.is(typeof res.royalty, 'number');
+        t.not(res.offchain, '');
+        t.not(res.tokenAmount, '');
+      });
+    };
+    const onErr: OnErr = (err: Error) => t.fail(err.message);
+    SplToken.findByOwner(owner, onOk, onErr, { sortable: Sortable.Asc });
+    await sleep(2);
+    end();
+  }),
+);
 
-  SplToken.findByOwner(owner, onOk, onErr, { isHolder: false });
-});
-
-test('Get token info owned with Asc', (t) => {
-  const onOk: OnOk<UserSideOutput.TokenMetadata> = (ok) => {
-    ok.forEach((res) => {
-      t.not(res.name, '');
-      t.not(res.mint, '');
-      t.not(res.symbol, '');
-      t.not(res.uri, '');
-      t.is(typeof res.royalty, 'number');
-      t.not(res.offchain, '');
-      t.not(res.tokenAmount, '');
-    });
-  };
-  const onErr: OnErr = (err: Error) => t.fail(err.message);
-
-  SplToken.findByOwner(owner, onOk, onErr, { sortable: Sortable.Asc });
-  t.pass();
-});
-
-test('Get token info by mint address', async (t) => {
-  (await SplToken.findByMint(mint)).match(
-    (ok: UserSideOutput.TokenMetadata) => {
-      t.not(ok.name, '');
-      t.not(ok.mint, '');
-      t.not(ok.symbol, '');
-      t.not(ok.uri, '');
-      t.not(ok.royalty, '');
-      t.not(ok.tokenAmount, '');
-      t.not(ok.offchain, '');
-    },
-    (err: Error) => t.fail(err.message),
-  );
-});
+test(
+  'Get token info by mint address',
+  withCallback(async (t: any, end: any) => {
+    (await SplToken.findByMint(mint)).match(
+      (ok: UserSideOutput.TokenMetadata) => {
+        t.log(ok);
+        t.not(ok.name, '');
+        t.not(ok.mint, '');
+        t.not(ok.symbol, '');
+        t.not(ok.uri, '');
+        t.not(ok.royalty, '');
+        t.not(ok.tokenAmount, '');
+        t.not(ok.offchain, '');
+      },
+      (err: Error) => t.fail(err.message),
+    );
+    await sleep(2);
+    end();
+  }),
+);
 
 test('[Error]Get token info by mint address, but token standard is difierent', async (t) => {
   (await SplToken.findByMint(nftMint)).match(
