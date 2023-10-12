fad22ca454e93981c0dda9f88e6627fc12067892
fukaoi <usapyon2022@gmail.com>
2023-10-12 23:44:35 +0900

    wip

--- packages/storage/src/provenance-layer.ts
+++ packages/storage/src/provenance-layer.ts
@@ -1,11 +1,33 @@
-import { Constants } from '~/shared';
+import { Constants, debugLog, isBrowser, isNode, Try } from '~/shared';
 import { FileContent } from '~/types/converter';
 import { PhantomProvider } from '~/types/phantom';
 import Irys, { WebIrys } from '@irys/sdk';
+import { UploadResponse } from '@irys/sdk/build/esm/common/types';
 
 export namespace ProvenanceLayer {
-  export const upload = () => {};
   const TOKEN = 'solana';
+
+  export const uploadFile = (
+    uploadFile: string | File,
+    identity: Secret | PhantomProvider,
+  ) => {
+    Try(async () => {
+      const irys = await getIrys(identity);
+      const tags = [{ name: 'application-id', value: 'MyNFTDrop' }];
+
+      const buffer = await toBuffer(uploadFile);
+      const willPay = await calculateCost(buffer.byteLength, identity);
+      const fundTx = await irys.fund(irys.utils.toAtomic(willPay));
+      debugLog('#fundTx: ', fundTx);
+      let receipt!: UploadResponse;
+      if (isNode() && typeof uploadFile === 'string') {
+        receipt = await (irys as Irys).uploadFile(uploadFile, { tags });
+      } else if (isBrowser() && typeof uploadFile !== 'string') {
+        receipt = await (irys as WebIrys).uploadFile(uploadFile, { tags });
+      }
+      return `https://gateway.irys.xyz/${receipt.id}`;
+    });
+  };
 
   // @internal
   export const toBuffer = async (
@@ -14,6 +36,9 @@ export namespace ProvenanceLayer {
     let buffer: ArrayBuffer;
     if (typeof content === 'string') {
       buffer = (await import('fs')).readFileSync(content);
+    } else if (content instanceof File) {
+      // todo File
+      buffer = content;
     } else if (isArrayBuffer(content)) {
       buffer = content;
     } else {
@@ -23,7 +48,14 @@ export namespace ProvenanceLayer {
   };
 
   // @internal
-  export const getIrys = async (secret: Secret) => {
+  export const getIrys = async (identity: Secret | PhantomProvider) => {
+    return isNode()
+      ? await getNodeIrys(identity as Secret)
+      : await getBrowserIrys(identity as PhantomProvider);
+  };
+
+  // @internal
+  export const getNodeIrys = async (secret: Secret) => {
     const url = Constants.BUNDLR_NETWORK_URL;
     const clusterUrl = Constants.currentCluster;
     const token = TOKEN;
@@ -52,5 +84,17 @@ export namespace ProvenanceLayer {
 
   const isArrayBuffer = (value: any): value is ArrayBuffer => {
     return value instanceof ArrayBuffer;
+  };
+
+  const calculateCost = async (
+    size: number,
+    identity: Secret | PhantomProvider,
+  ) => {
+    const irys = await getIrys(identity);
+    const priceAtomic = await irys.getPrice(size);
+    const priceConverted = irys.utils.fromAtomic(priceAtomic);
+    debugLog('#size: ', size);
+    debugLog(`#price: ${priceConverted}`);
+    return priceConverted;
   };
 }
--- packages/types/src/converter/common.ts
+++ packages/types/src/converter/common.ts
@@ -2,7 +2,7 @@ import BN from 'bn.js';
 
 export type Option<T> = T | null;
 export type bignum = number | BN;
-export type FileContent = string | Buffer | Uint8Array | ArrayBuffer;
+export type FileContent = string | Buffer | Uint8Array | ArrayBuffer | File;
 
 export namespace Common {
   export type Properties = {
