385f82284ae150fdc4f587cffd1178857e112ccf
fukaoi <usapyon2022@gmail.com>
2023-11-01 15:38:26 +0900

    wip

--- packages/suite-compressed-nft/src/collection.ts
+++ packages/suite-compressed-nft/src/collection.ts
@@ -14,12 +14,12 @@ import {
   TOKEN_PROGRAM_ID,
 } from '@solana/spl-token';
 import { debugLog, Try } from '~/shared';
+import { Converter } from '~/converter';
+import { Storage } from '~/storage';
 import { Validator } from '~/validator';
-import { Node } from '~/node';
-import { Instruction } from '~/instruction';
+import { MintInstruction } from '~/instruction';
+import { InputNftMetadata } from '~/types/regular-nft';
 import { KeypairAccount } from '@solana-suite/spl-token';
-import { RegularNft } from '~/suite-regular-nft';
-// import { SplToken } from '~/suite-spl-token';
 
 /**
  * create a collection
@@ -28,112 +28,107 @@ import { RegularNft } from '~/suite-regular-nft';
  * @param {feePayer} Secret
  * @return Promise<Result<Instruction, Error>>
  */
-// export namespace CompressedNft {
-//   export const mintCollection = (
-//     owner: Pubkey,
-//     signer: Secret,
-//     input: UserSideInput.NftMetadata,
-//     feePayer?: Secret,
-//     freezeAuthority?: Pubkey,
-//   ) => {
-//     return Try(async () => {
-//       const valid = Validator.checkAll<UserSideInput.NftMetadata>(input);
-//       if (valid.isErr) {
-//         throw valid.error;
-//       }
-//
-//       const payer = feePayer ? feePayer : signer;
-//
-//       //--- porperties, Upload content ---
-//       let properties;
-//       if (input.properties && input.storageType) {
-//         properties = await Converter.Properties.intoInfraSide(
-//           input.properties,
-//           Storage.uploadFile,
-//           input.storageType,
-//           payer,
-//         );
-//       } else if (input.properties && !input.storageType) {
-//         throw Error('Must set storageType if will use properties');
-//       }
-//
-//       input = {
-//         ...input,
-//         properties,
-//       };
-//       //--- porperties, Upload content ---
-//
-//       const nftStorageMetadata = Storage.toConvertOffchaindata(input, 0);
-//
-//       // created at by unix timestamp
-//       const createdAt = Math.floor(new Date().getTime() / 1000);
-//       nftStorageMetadata.created_at = createdAt;
-//
-//       let uri!: string;
-//       if (input.filePath && input.storageType) {
-//         const uploaded = await Storage.upload(
-//           nftStorageMetadata,
-//           input.filePath,
-//           input.storageType,
-//           payer,
-//         );
-//         debugLog('# upload content url: ', uploaded);
-//         if (uploaded.isErr) {
-//           throw uploaded;
-//         }
-//         uri = uploaded.value;
-//       } else if (input.uri) {
-//         uri = input.uri;
-//       } else {
-//         throw Error(`Must set 'storageType + filePath' or 'uri'`);
-//       }
-//
-//       let datav2 = Converter.NftMetadata.intoInfraSide(
-//         input,
-//         uri,
-//         sellerFeeBasisPoints,
-//       );
-//
-//       //--- collection ---
-//       let collection;
-//       if (input.collection && input.collection) {
-//         collection = Converter.Collection.intoInfraSide(input.collection);
-//         datav2 = { ...datav2, collection };
-//       }
-//
-//       const isMutable = input.isMutable === undefined ? true : input.isMutable;
-//
-//       debugLog('# input: ', input);
-//       debugLog('# sellerFeeBasisPoints: ', sellerFeeBasisPoints);
-//       debugLog('# datav2: ', datav2);
-//
-//       const mint = KeypairAccount.create();
-//
-//       const insts = await createMintInstructions(
-//         mint.toPublicKey(),
-//         owner.toPublicKey(),
-//         datav2,
-//         payer.toKeypair().publicKey,
-//         isMutable,
-//       );
-//
-//       // freezeAuthority
-//       if (freezeAuthority) {
-//         insts.push(
-//           createDeleagateInstruction(
-//             mint.toPublicKey(),
-//             owner.toPublicKey(),
-//             freezeAuthority.toPublicKey(),
-//           ),
-//         );
-//       }
-//
-//       return new MintInstruction(
-//         insts,
-//         [signer.toKeypair(), mint.toKeypair()],
-//         payer.toKeypair(),
-//         mint.pubkey,
-//       );
-//     });
-//   };
-// }
+export namespace CompressedNft {
+  export const mintCollection = (
+    owner: Pubkey,
+    signer: Secret,
+    input: InputNftMetadata,
+    feePayer?: Secret,
+    freezeAuthority?: Pubkey,
+  ) => {
+    return Try(async () => {
+      const valid = Validator.checkAll<InputNftMetadata>(input);
+      if (valid.isErr) {
+        throw valid.error;
+      }
+
+      const payer = feePayer ? feePayer : signer;
+
+      //--- porperties, Upload content ---
+      let properties;
+      if (input.properties && input.storageType) {
+        properties = await Converter.Properties.intoInfra(
+          input.properties,
+          Storage.uploadFile,
+          input.storageType,
+          payer,
+        );
+      } else if (input.properties && !input.storageType) {
+        throw Error('Must set storageType if will use properties');
+      }
+
+      input = {
+        ...input,
+        properties,
+      };
+      //--- porperties, Upload content ---
+
+      const nftStorageMetadata = Storage.toConvertOffchaindata(input, 0);
+
+      // created at by unix timestamp
+      const createdAt = Math.floor(new Date().getTime() / 1000);
+      nftStorageMetadata.created_at = createdAt;
+
+      let uri!: string;
+      if (input.filePath && input.storageType) {
+        const uploaded = await Storage.upload(
+          nftStorageMetadata,
+          input.filePath,
+          input.storageType,
+          payer,
+        );
+        debugLog('# upload content url: ', uploaded);
+        if (uploaded.isErr) {
+          throw uploaded;
+        }
+        uri = uploaded.value;
+      } else if (input.uri) {
+        uri = input.uri;
+      } else {
+        throw Error(`Must set 'storageType + filePath' or 'uri'`);
+      }
+
+      let datav2 = Converter.NftMetadata.intoInfra(input, uri, 0);
+
+      //--- collection ---
+      let collection;
+      if (input.collection && input.collection) {
+        collection = Converter.Collection.intoInfra(input.collection);
+        datav2 = { ...datav2, collection };
+      }
+
+      const isMutable = input.isMutable === undefined ? true : input.isMutable;
+
+      debugLog('# input: ', input);
+      debugLog('# datav2: ', datav2);
+
+      const mint = KeypairAccount.create();
+
+      const insts = await createMintInstructions(
+        mint.toPublicKey(),
+        owner.toPublicKey(),
+        datav2,
+        payer.toKeypair().publicKey,
+        isMutable,
+      );
+
+      // freezeAuthority
+      if (freezeAuthority) {
+        insts.push(
+          createDeleagateInstruction(
+            mint.toPublicKey(),
+            owner.toPublicKey(),
+            freezeAuthority.toPublicKey(),
+          ),
+        );
+      }
+
+      return new MintInstruction(
+        insts,
+        [signer.toKeypair(), mint.toKeypair()],
+        payer.toKeypair(),
+        mint.pubkey,
+      );
+    });
+  };
+}
