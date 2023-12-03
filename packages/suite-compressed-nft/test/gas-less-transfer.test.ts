513f722e2466ac6dcc57903549a5887153bd0bb5
fukaoi <usapyon2022@gmail.com>
2023-12-03 15:09:54 +0900

    updated

--- packages/suite-compressed-nft/test/gas-less-transfer.test.ts
+++ packages/suite-compressed-nft/test/gas-less-transfer.test.ts
@@ -18,9 +18,15 @@ test('Fee-Less Transfer nft', async (t) => {
   const assets = await CompressedNft.findByOwner(source.pubkey);
 
   if (assets.isErr) {
+    console.error(assets);
     t.fail(assets.error.message);
+  } else if (assets.isOk && assets.value.metadatas.length < 1) {
+    t.fail(
+      'Your assets is empty'
+    );
   }
 
+  console.log(assets);
   const mint = assets.unwrap().metadatas[0].mint;
   t.log('# mint: ', mint);
 
@@ -35,24 +41,3 @@ test('Fee-Less Transfer nft', async (t) => {
   t.true(res.isOk, `${res.unwrap()}`);
   t.log('# tx signature: ', res.unwrap());
 });
-
-// test('[Error] No match delegate(or feePayer)', async (t) => {
-//   const assets = await CompressedNft.findByOwner(source.pubkey);
-//   const noMatchDelegate = Account.Keypair.create();
-//
-//   if (assets.isErr) {
-//     t.fail(assets.error.message);
-//   }
-//
-//   const mint = assets.unwrap().metadatas[0].mint;
-//   t.log('# mint: ', mint);
-//
-//   const serialized = await CompressedNft.gasLessTransfer(
-//     mint,
-//     source.pubkey,
-//     dest.pubkey,
-//     noMatchDelegate.pubkey,
-//   );
-//
-//   t.true(serialized.isErr);
-// });
