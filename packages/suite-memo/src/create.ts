76b7f3a1f790472e82fd9c7cf3efc244fabd2671
fukaoi <usapyon2022@gmail.com>
2023-12-19 00:22:50 +0900

    replaced by suite-memo

--- packages/suite-memo/src/create.ts
+++ packages/suite-memo/src/create.ts
@@ -1,7 +1,7 @@
 import { TransactionInstruction } from '@solana/web3.js';
 import { TransactionBuilder } from '~/transaction-builder';
 import { Constants, Result, Try } from '~/shared';
-import { Pubkey, Secret } from '~/types/account';
+import { Secret } from '~/types/account';
 import { MemoOptions } from '~/types/memo';
 import bs from 'bs58';
 import { CommonStructure } from '~/types/transaction-builder';
@@ -12,19 +12,26 @@ export namespace Memo {
 
   export const encode = (data: string): Buffer => Buffer.from(data);
 
+  /**
+   * Create memo
+   *
+   * @param {string} data    //  memo data
+   * @param {Secret} owner   //  memo owner
+   * @param {Partial<DelegateOptions>} options
+   * @return Promise<Result<Transaction, Error>>
+   */
   export const create = (
     data: string,
-    owner: Pubkey,
-    signer: Secret,
+    owner: Secret,
     options: Partial<MemoOptions> = {},
   ): Result<CommonStructure, Error> => {
     return Try(() => {
       const feePayer = options.feePayer;
 
-      const key = owner.toPublicKey()
+      const key = owner.toKeypair().publicKey
         ? [
             {
-              pubkey: owner.toPublicKey(),
+              pubkey: owner.toKeypair().publicKey,
               isSigner: true,
               isWritable: true,
             },
@@ -37,11 +44,11 @@ export namespace Memo {
         keys: key,
       });
 
-      const payer = feePayer || signer;
+      const payer = feePayer || owner;
 
       return new TransactionBuilder.Common(
         [instruction],
-        [signer.toKeypair()],
+        [owner.toKeypair()],
         payer.toKeypair(),
       );
     });
