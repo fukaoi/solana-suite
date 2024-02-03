fd7ab0e6415384eb4b1fc2f3df08fd2805bcfc2e
fukaoi <usapyon2022@gmail.com>
2024-02-03 13:19:14 +0900

    No need function

--- packages/account/src/index.ts
+++ packages/account/src/index.ts
@@ -1,9 +1,7 @@
-import { Account as Associated } from './associated';
 import { Account as Keypair } from './keypair';
 import { Account as Pda } from './pda';
 
 export const Account = {
-  ...Associated,
   ...Keypair,
   ...Pda,
 };
