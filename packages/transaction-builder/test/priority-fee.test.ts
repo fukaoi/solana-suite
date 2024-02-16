da73be6bf185b30a3d4c9a6d1b06af6fba5c8e4e
fukaoi <usapyon2022@gmail.com>
2024-02-16 14:07:47 +0900

    Fix

--- packages/transaction-builder/test/priority-fee.test.ts
+++ packages/transaction-builder/test/priority-fee.test.ts
@@ -1,9 +1,11 @@
 import test from 'ava';
 import { CompressedNft } from '~/suite-compressed-nft';
+import { SplToken } from '~/suite-spl-token';
 import { SolNative } from '~/suite-sol-native';
 import { KeypairAccount } from '~/types/account';
 import { Pubkey } from '~/types/account';
 import { Setup } from 'test-tools/setup';
+import { RandomAsset } from 'test-tools/setupAsset';
 import { DasApi } from '~/das-api';
 
 let source: KeypairAccount;
@@ -102,27 +104,43 @@ test('[PartialSignStructure]Set priority fee', async (t) => {
   );
 });
 
-test('[BatchStructure]Set priority fee', async (t) => {
-  const solAmount = 0.01;
+test.only('[BatchStructure]Set priority fee', async (t) => {
+  const TOKEN_METADATA = {
+    name: 'solana-suite-token',
+    symbol: 'SST',
+    royalty: 50,
+    filePath: RandomAsset.get().filePath as string,
+    isMutable: false,
+  };
+
+  const MINT_DECIMAL = 3;
+  const inst1 = await SplToken.mint(
+    source.secret,
+    10000,
+    MINT_DECIMAL,
+    TOKEN_METADATA,
+    { feePayer: feePayer.secret },
+  );
+
+  t.true(inst1.isOk, `${inst1.unwrap()}`);
+  const token = inst1.unwrap().data as string;
+  t.log('# mint: ', token);
+
+  const burnAmount = 500;
+  const inst2 = SplToken.burn(
+    token,
+    source.pubkey,
+    [source.secret],
+    burnAmount,
+    MINT_DECIMAL,
+    { feePayer: feePayer.secret },
+  );
+
+  t.true(inst2.isOk);
+
   const testUri =
     'https://mainnet.helius-rpc.com/?api-key=15319bf4-5b40-4958-ac8d-6313aa55eb92';
   DasApi.changeDasUri(testUri);
-  const serialized = await SolNative.gasLessTransfer(
-    source.secret,
-    dest.pubkey,
-    solAmount,
-    feePayer.pubkey,
-    { isPriorityFee: true },
-  );
-
-  (await serialized.submit({ feePayer: feePayer.secret })).match(
-    (ok) => {
-      t.log('# tx signature: ', ok);
-      t.pass();
-    },
-    (err) => {
-      console.error(err);
-      t.fail(err.message);
-    },
-  );
+  const sig = await [inst1, inst2].submit({isPriorityFee: true});
+  t.log('# signature: ', sig.unwrap());
 });
