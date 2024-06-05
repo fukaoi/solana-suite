1a55d11e18a9f28bab923204def8befab4c1be30
fukaoi <usapyon2022@gmail.com>
2024-06-05 15:28:44 +0900

    init

---
+++ packages/suite-storage/src/filebase.ts
@@ -0,0 +1,65 @@
+import { Blob, NFTStorage } from 'nft.storage';
+import { Constants, debugLog, Result, Try, unixTimestamp } from '~/suite-utils';
+import { ProvenanceLayer } from './provenance-layer';
+import { FileType, Offchain } from '~/types/storage';
+
+// @internal
+export namespace Filebase {
+  const createGatewayUrl = (cid: string): string =>
+    `${Constants.FILEBADE_GATEWAY_URL}/${cid}`;
+
+  const connect = () => {
+    return new NFTStorage({ token: Constants.NFT_STORAGE_API_KEY });
+  };
+
+  export const uploadFile = async (
+    fileType: FileType,
+  ): Promise<Result<string, Error>> => {
+    return Try(async () => {
+      debugLog('# upload content: ', fileType);
+      let file!: Buffer;
+      if (ProvenanceLayer.isNodeable(fileType)) {
+        file = (await import('fs')).readFileSync(fileType);
+      } else if (ProvenanceLayer.isBrowserable(fileType)) {
+        file = Buffer.from(await fileType.arrayBuffer());
+      } else {
+        file = Buffer.from(fileType as ArrayBuffer);
+      }
+
+      const blobImage = new Blob([file]);
+      const res = await connect().storeBlob(blobImage);
+      return createGatewayUrl(res);
+    });
+  };
+
+  /**
+   * Upload content
+   *
+   * @param {Offchain} storageData
+   * {
+   *   name?: {string}                      // nft content name
+   *   symbol?: {string}                    // nft ticker symbol
+   *   description?: {string}               // nft content description
+   *   sellerFeeBasisPoints?: number        // royalty percentage
+   *   image?: {string}                     // uploaded uri of original content
+   *   external_url?: {string}              // landing page, home page uri, related url
+   *   attributes?: {JsonMetadataAttribute[]}     // game character parameter, personality, characteristics
+   *   properties?: {JsonMetadataProperties<Uri>} // included file name, uri, supported file type
+   *   collection?: Collection              // collections of different colors, shapes, etc.
+   *   [key: string]: {unknown}             // optional param, Usually not used.
+   * }
+   * @return Promise<Result<string, Error>>
+   */
+  export const uploadData = async (
+    storageData: Offchain,
+  ): Promise<Result<string, Error>> => {
+    return Try(async () => {
+      // created at by unix timestamp
+      storageData.created_at = unixTimestamp();
+      debugLog('# Will upload offchain: ', storageData);
+      const blobJson = new Blob([JSON.stringify(storageData)]);
+      const res = await connect().storeBlob(blobJson);
+      return createGatewayUrl(res);
+    });
+  };
+}
--- packages/suite-utils/src/constants.ts
+++ packages/suite-utils/src/constants.ts
@@ -158,6 +158,7 @@ export namespace Constants {
   export const MAX_TRANSACTION_VERSION: number = 0;
   export const MAX_TRANSACTION_RETRIES = 1;
   export const NFT_STORAGE_GATEWAY_URL = 'https://ipfs.io/ipfs';
+  export const FILEBADE_GATEWAY_URL = 'https://ipfs.filebase.io/ipfs/';
   export const IRYS_GATEWAY_URL = 'https://gateway.irys.xyz';
   export const BUNDLR_NETWORK_URL = switchBundlr(Config.cluster.type);
   export const DAS_API_URL = switchDasApi(Config.cluster.type);
