493e0b3fc4a9617d64dcf37b550a2a9c9be638fc
fukaoi <usapyon2022@gmail.com>
2024-06-17 15:14:03 +0900

    updated

--- packages/suite-utils/test/config.test.ts
+++ packages/suite-utils/test/config.test.ts
@@ -3,4 +3,10 @@ import { Config, Constants } from '../src/';
 
 test('Load config', (t) => {
   t.log(Constants.loadConfig());
+  t.pass();
+});
+
+test('Update config data', (t) => {
+  t.log(Constants.loadConfig());
+  t.pass();
 });
