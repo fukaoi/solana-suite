9d3efceee67a7a1079e08a52818eafbef7d71590
fukaoi <usapyon2022@gmail.com>
2024-06-18 16:02:07 +0900

    Did pre puboish

--- packages/suite-config/test/config-script.test.ts
+++ packages/suite-config/test/config-script.test.ts
@@ -5,10 +5,10 @@ test('exec solana-suite-config script', (t) => {
   const res = execSync('pnpm tsx ./src/config-script.ts -s');
   t.log(res.toString());
   t.pass();
-});  
+});
 
 // test('set filebase parameter', (t) => {
 //   const res = execSync('pnpm tsx ./src/config-script.ts -f key secret');
 //   t.log(res.toString());
 //   t.pass();
-// }); 
+// });
